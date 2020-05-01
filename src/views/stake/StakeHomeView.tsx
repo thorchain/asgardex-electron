9dd03ac0bff85a435d8c053dfc3e02d943946a1c
Veado <veado@protonmail.com>
2020-05-01 20:00:50 +0200

    PoolsContext

--- src/views/stake/StakeHomeView.tsx
+++ src/views/stake/StakeHomeView.tsx
@@ -3,11 +3,16 @@ import { Button } from 'antd'
 import { useHistory } from 'react-router-dom'
 import { stakeRoute } from '../../routes'
 import View from '../View'
+import { usePoolsContext } from '../../contexts/PoolsContext'
+import { useObservableState } from 'observable-hooks'
 
 type Props = {}
 
 const StakeHomeView: React.FC<Props> = (_): JSX.Element => {
   const history = useHistory()
+
+  const pools$ = usePoolsContext()
+  const pools = useObservableState(pools$)
 
   const clickHandler = (asset: string) => {
     history.push(stakeRoute.path({ asset }))
@@ -16,6 +21,7 @@ const StakeHomeView: React.FC<Props> = (_): JSX.Element => {
   return (
     <View>
       <h1>Stake Home</h1>
+      <h2>{JSON.stringify(pools)}</h2>
       <Button onClick={() => clickHandler('BNB')}>BNB</Button>
       <Button onClick={() => clickHandler('TUSDB-000')}>TUSDB</Button>
     </View>
--- src/views/swap/SwapHomeView.tsx
+++ src/views/swap/SwapHomeView.tsx
@@ -3,6 +3,8 @@ import { Button } from 'antd'
 import { useHistory } from 'react-router-dom'
 import { swapRoute, SwapRouteParams } from '../../routes'
 import View from '../View'
+import { useObservableState } from 'observable-hooks'
+import { usePoolsContext } from '../../contexts/PoolsContext'
 
 type Props = {}
 
@@ -12,10 +14,13 @@ const SwapHomeView: React.FC<Props> = (_): JSX.Element => {
   const clickHandler = (p: SwapRouteParams) => {
     history.push(swapRoute.path(p))
   }
+  const pools$ = usePoolsContext()
+  const pools = useObservableState(pools$)
 
   return (
     <View>
       <h1>Swap Home</h1>
+      <h2>{JSON.stringify(pools)}</h2>
       <Button onClick={() => clickHandler({ source: 'rune', target: 'bnb' })}>RUNE -&gt; BNB</Button>
       <Button onClick={() => clickHandler({ source: 'rune', target: 'tusdb' })}>RUNE -&gt; TUSDB</Button>
     </View>
