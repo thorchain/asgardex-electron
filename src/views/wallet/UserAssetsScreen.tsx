79794d83d2904e24e02c3c83521cef25dd85a047
Robert McLeod <robert@datawallet.com>
2020-05-08 15:22:47 +0200

    Adding data model types

--- src/views/wallet/UserAssetsScreen.tsx
+++ src/views/wallet/UserAssetsScreen.tsx
@@ -3,14 +3,24 @@ import { Row, Col, Typography, Table } from 'antd'
 import DynamicCoin from '../../components/shared/CoinIcons/DynamicCoin'
 const { Title } = Typography
 
+type UserAssetType = {
+  _id: string;
+  free: number;
+  frozen: number;
+  locked: number;
+  symbol: string;
+  name: string;
+  value: string;
+}
+
 // Dummy data
-const UserAssets:any = [
-  { _id: '1', free: '99', frozen: '11', locked: '21', symbol: 'BNB-JST', name: "Binance", value:"0.99"},
-  { _id: '2', free: '1034', frozen: '38', locked: '101', symbol: 'RUN-1E0', name: "Rune", value:"0.25"}
+const UserAssets:UserAssetType[] = [
+  { _id: '1', free: 99, frozen: 11, locked: 21, symbol: 'BNB-JST', name: "Binance", value:"0.99"},
+  { _id: '2', free: 1034, frozen: 38, locked: 101, symbol: 'RUN-1E0', name: "Rune", value:"0.25"}
 ]
 
 const UserAssetsScreen: React.FC = (): JSX.Element => {
-  const [assets, setAssets] = useState([])
+  const [assets, setAssets] = useState<UserAssetType[]>([])
 
   async function setData() {
     const res = UserAssets
--- src/views/wallet/UserStakesScreen.tsx
+++ src/views/wallet/UserStakesScreen.tsx
@@ -1,19 +1,55 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import { Row, Col, Typography, Table } from 'antd'
+import DynamicCoin from '../../components/shared/CoinIcons/DynamicCoin'
 const { Title } = Typography
 
-const UserAssets:any = [
-  { _id: '1', free: '99', frozen: '11', locked: '21', symbol: 'BNB-JST' },
-  { _id: '2', free: '1034', frozen: '38', locked: '101', symbol: 'RUN-1E0' }
+type UserAssetType = {
+  _id: string;
+  free: number;
+  frozen: number;
+  locked: number;
+  symbol: string;
+  name: string;
+  value: string;
+}
+
+// Dummy data
+const UserAssets:UserAssetType[] = [
+  { _id: '1', free: 99, frozen: 11, locked: 21, symbol: 'BNB-JST', name: "Binance", value:"0.99"},
+  { _id: '2', free: 1034, frozen: 38, locked: 101, symbol: 'RUN-1E0', name: "Rune", value:"0.25"}
 ]
 
-export default function UserStakesScreen (): JSX.Element {
+const UserStakesScreen: React.FC = (): JSX.Element => {
+  const [assets, setAssets] = useState<UserAssetType[]>([])
+
+  async function setData() {
+    const res = UserAssets
+    if (res.length > 0) {
+      setAssets(res)
+    }
+  }
+  useEffect(() => {
+    setData()
+  },[])
   return (
     <Row>
       <Col span={24}>
         <Title level={4}>User Stakes</Title>
+        <Table dataSource={assets} pagination={false}>
+          <Table.Column title="Icon" dataIndex="symbol" key="icon" render={symbol => (
+            <>
+              <DynamicCoin type={symbol} size={'normal'}/>
+            </>
+          )}/>
+          <Table.Column title="Name" dataIndex="name" key="name" />
+          <Table.Column title="Symbol" dataIndex="symbol" key="symbol" />
+          <Table.Column title="Frozen" dataIndex="frozen" key="frozen" />
+          <Table.Column title="Value" dataIndex="value" key="value" width={1}/>
+        </Table>
+
       </Col>
     </Row>
   )
 }
+
+export default UserStakesScreen
