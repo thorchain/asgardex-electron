bd66369225dfc906643acfc449125c6960190b9c
Robert McLeod <robert@datawallet.com>
2020-05-11 11:49:33 +0200

    Extract rgb2hex function

--- src/helpers/colorHelpers.ts
+++ src/helpers/colorHelpers.ts
@@ -1,18 +1,19 @@
 export const rainbowStop = (val: string) => {
   const h: number = parseFloat(val)
   const f = (n: number, k = (n + h * 12) % 12) => 0.5 - 0.5 * Math.max(Math.min(k - 3, 9 - k, 1), -1)
-  const rgb2hex = (r: number, g: number, b: number) =>
-    '#' +
-    [r, g, b]
-      .map((x) =>
-        Math.round(x * 255)
-          .toString(16)
-          .padStart(2, '0')
-      )
-      .join('')
-
   return rgb2hex(f(0), f(8), f(4))
 }
+
+export const rgb2hex = (r: number, g: number, b: number) =>
+  '#' +
+  [r, g, b]
+    .map((x) =>
+      Math.round(x * 255)
+        .toString(16)
+        .padStart(2, '0')
+    )
+    .join('')
+
 
 export const getIntFromName = (str: string): string[] => {
   const inputStr = String(str).toUpperCase()
