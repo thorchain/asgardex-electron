1b7df92d0fa4776ec4af9154d69e25804a1884c1
Robert McLeod <robert@datawallet.com>
2020-05-11 18:02:28 +0200

    Refactor helper method into standalone

--- src/components/wallet/UserTransactionsTable.tsx
+++ src/components/wallet/UserTransactionsTable.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shortSymbol } from '../../helpers/tokenHelpers'
+import { txParty } from '../../helpers/transactionHelpers'
 import { Typography, Table } from 'antd'
 const { Text } = Typography
 
@@ -29,29 +30,7 @@ type Props = { transactions: UserTransactionTypes[] }
 const TransactionsTable: React.FC<Props> = ({ transactions }): JSX.Element => {
   const address = 'tbnb1vxutrxadm0utajduxfr6wd9kqfalv0dg2wnx5y'
   const party = (tx: UserTransactionTypes) => {
-    const from = tx.fromAddr
-    const to = tx.toAddr
-    switch (tx.txType) {
-      case 'TRANSFER':
-        if (from === address) {
-          return { msg: 'send', label: 'to', address: to, color: 'error', op: '-' }
-        } else {
-          return { msg: 'receive', label: 'from', address: from, color: 'success', op: '+' }
-        }
-      case 'FREEZE_TOKEN':
-        return { msg: 'freeze', label: 'from', address: from, color: 'info', op: '-' }
-      case 'UN_FREEZE_TOKEN':
-        return { msg: 'unfreeze', label: 'to', address: from, color: 'warning', op: '+' }
-      case 'outboundTransferInfo':
-        if (from === address) {
-          return { msg: 'pending', label: 'to', address: to, color: 'secondary', op: '-' }
-        } else {
-          return { msg: 'pending', label: 'from', address: from, color: 'secondary', op: '+' }
-        }
-
-      default:
-        return { msg: 'pending', label: '', address: '', color: '', op: '' }
-    }
+    return txParty(address, tx)
   }
   return (
     <Table size="small" dataSource={transactions} rowKey="_id" pagination={false}>
