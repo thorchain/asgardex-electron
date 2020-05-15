4e193091c8c01a91d7714fb982ca5fef33133d92
codementor <skyorion427@gmail.com>
2020-05-15 06:38:06 -0400

    Setup Storybook (#61)

---
+++ src/components/Header.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Header from './Header'
+
+storiesOf('Components/Header', module).add('default', () => {
+  return <Header />
+})
