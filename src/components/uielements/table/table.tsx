7fca7f48d34093bb1aaf7be277b4aa29bf0b535e
codementor <skyorion427@gmail.com>
2020-05-20 11:45:28 -0400

    Add Table Component (#82)

---
+++ src/components/uielements/table/table.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import { TableProps } from 'antd/lib/table'
+
+import { TableWrapper } from './table.style'
+
+export type Props = {
+  className?: string
+}
+
+const Table: React.FC<Props & TableProps<any>> = (props: Props): JSX.Element => {
+  const { className = '', ...otherProps } = props
+
+  return <TableWrapper className={`table-wrapper ${className}`} pagination={false} {...otherProps} />
+}
+
+export default Table
