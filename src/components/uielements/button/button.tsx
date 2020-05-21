8cb85d0681a1e5b04f3de7793a525f23a45bbf15
codementor <skyorion427@gmail.com>
2020-05-21 12:23:30 -0400

    Add Button component and fix issues in the label (#86)

---
+++ src/components/uielements/button/button.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { ButtonProps } from 'antd/lib/button'
+import { ButtonColor, ButtonSize, ButtonWeight, ButtonType } from './types'
+
+import { ButtonWrapper } from './button.style'
+
+type ComponentProps = {
+  sizevalue?: ButtonSize
+  color?: ButtonColor
+  weight?: ButtonWeight
+  typevalue?: ButtonType
+  focused?: boolean
+  round?: boolean
+  className?: string
+}
+
+export type Props = ComponentProps & ButtonProps
+
+const Button: React.FC<Props> = (props: Props): JSX.Element => {
+  const {
+    children,
+    sizevalue = 'normal',
+    color = 'primary',
+    typevalue = 'default',
+    weight = '500',
+    round = false,
+    focused = false,
+    className = '',
+    ...otherProps
+  } = props
+
+  const focusedStyle = focused ? 'focused' : ''
+
+  return (
+    <ButtonWrapper
+      className={`${className} btn-wrapper ${focusedStyle}`}
+      type="primary"
+      weight={weight}
+      color={color}
+      sizevalue={sizevalue}
+      typevalue={typevalue}
+      round={round}
+      {...otherProps}>
+      {children}
+      {props.typevalue === 'normal' && <div className="borderBottom" />}
+    </ButtonWrapper>
+  )
+}
+export default Button
