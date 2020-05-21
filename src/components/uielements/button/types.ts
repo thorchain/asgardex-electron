3325b4e222de818f560797332e44aa02e215456f
skyorion427 <skyorion427@gmail.com>
2020-05-21 12:13:04 -0400

    Add Button component and fix issues in the label

---
+++ src/components/uielements/button/types.ts
@@ -0,0 +1,4 @@
+export type ButtonSize = 'small' | 'normal' | 'big'
+export type ButtonColor = 'primary' | 'success' | 'warning' | 'error'
+export type ButtonWeight = 'normal' | 'bold' | '500'
+export type ButtonType = 'normal' | 'default' | 'outline' | 'ghost'
--- src/components/uielements/label/label.stories.tsx
+++ src/components/uielements/label/label.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text, radios } from '@storybook/addon-knobs'
-import { keyBy } from 'lodash'
 
 import Label from './label'
 import { Color, Size } from './types'
@@ -21,18 +20,28 @@ type ColorOptions = {
 storiesOf('Components/Label', module).add('properties', () => {
   const labelText = text('Label Text', 'This is Label Text!')
 
-  const sizeOptions: SizeOptions = keyBy(['tiny', 'small', 'normal', 'big', 'large'])
-  const colorOptions: ColorOptions = keyBy([
-    'primary',
-    'success',
-    'warning',
-    'error',
-    'normal',
-    'light',
-    'dark',
-    'white'
-  ])
-  const weightOptions: WeightOptions = keyBy(['light', 'bold', 'normal'])
+  const sizeOptions: SizeOptions = {
+    tiny: 'tiny',
+    small: 'small',
+    normal: 'normal',
+    big: 'big',
+    large: 'large'
+  }
+  const colorOptions: ColorOptions = {
+    primary: 'primary',
+    success: 'success',
+    warning: 'warning',
+    error: 'error',
+    normal: 'normal',
+    light: 'light',
+    dark: 'dark',
+    white: 'white'
+  }
+  const weightOptions: WeightOptions = {
+    light: 'light',
+    bold: 'bold',
+    normal: 'normal'
+  }
 
   const size = radios('size', sizeOptions, 'normal')
   const weight = radios('weight', weightOptions, 'normal')
