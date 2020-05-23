9c244a7a3eee36c0e5c76896ee56f4ef2bf948f9
Robert McLeod <robert@datawallet.com>
2020-05-23 07:41:31 +0200

    Add initial keystore, and datastore

--- src/components/wallet/forms/ImportMnemonicForm.tsx
+++ src/components/wallet/forms/ImportMnemonicForm.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useCallback } from 'react'
+import { crypto } from '@binance-chain/javascript-sdk'
+import * as BIP39 from 'bip39'
+import { KeyStore } from '@binance-chain/javascript-sdk/typings/crypto'
 
 import { Form, Input, Button } from 'antd'
 import { Store } from 'antd/lib/form/interface'
@@ -12,14 +15,31 @@ const ImportMnemonicForm: React.FC = (): JSX.Element => {
     const network = 'testnet'
     console.log('importing mnemonic...')
     console.log(`network: ${network}; mnemonic: ${mnemonic}; password: ${password}`)
+    const privKey: string = crypto.getPrivateKeyFromMnemonic(mnemonic)
+    const keystore: KeyStore = crypto.generateKeyStore(privKey, password)
+    const address: string = crypto.getAddressFromPrivateKey(privKey)
+    // Temporary store during development
+    localStorage.setItem('address', address)
+    localStorage.setItem('keystore', JSON.stringify(keystore))
   }
 
+  const checkMnemonic = (_: any, value: any) => {
+    console.log('validating something?')
+    // console.log(rule)
+    console.log(value)
+    if (!value) {
+      return Promise.reject('required tettster')
+    }
+    return BIP39.validateMnemonic(value) ? Promise.resolve() : Promise.reject('Invalid mnemonic seed phrase')
+  }
+  const checkPasswords = (_: any, value: any) => {
+    console.log('checking passwords')
+  }
   const handleImportFormSubmit = useCallback(async (vals: Store) => {
     setLoadingMsg('Generating wallet')
     // Delay to allow for UI render DOM update before CPU takes over keystore/cryto processing
     await delay(200)
     try {
-      console.log('trying...')
       importMnemonicWallet(vals.mnemonic, vals.password)
     } catch (err) {
       setLoadingMsg('')
@@ -28,13 +48,36 @@ const ImportMnemonicForm: React.FC = (): JSX.Element => {
   }, [])
   return (
     <Form form={form} onFinish={handleImportFormSubmit} labelCol={{ span: 24 }}>
-      <Form.Item name="mnemonic" label="Mnemonic (Phrase)">
+      <Form.Item
+        name="mnemonic"
+        label="Mnemonic (Phrase)"
+        rules={[{ required: true, validator: checkMnemonic }]}
+        validateTrigger={['onSubmit', 'onChange']}>
         <Input size="large" type="text" />
       </Form.Item>
-      <Form.Item name="password" label="New Password">
+      <Form.Item
+        name="password"
+        label="New Password"
+        rules={[{ required: true }]}
+        validateTrigger={['onSubmit', 'onChange']}>
         <Input size="large" type="password" />
       </Form.Item>
-      <Form.Item name="repeatPassword" label="Repeat Password">
+      <Form.Item
+        name="repeatPassword"
+        label="Repeat Password"
+        dependencies={['password']}
+        validateTrigger={['onSubmit', 'onBlur']}
+        rules={[
+          { required: true },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve()
+              }
+              return Promise.reject('Password mismatch!')
+            }
+          })
+        ]}>
         <Input size="large" type="password" />
       </Form.Item>
       <Form.Item>
