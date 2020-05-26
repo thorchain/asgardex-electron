a101c7156c85de53a851a2374a548c6034807862
FrozenIce0617 <prosdev087@gmail.com>
2020-05-26 16:46:05 +0100

    Add slider component

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { SliderProps } from 'antd/lib/slider'
+import { TooltipPlacement } from 'antd/lib/tooltip'
+
+import { SliderWrapper, SliderLabel } from './Slider.style'
+
+interface Props extends SliderProps {
+  className?: string
+  tooltipPlacement?: TooltipPlacement
+  withLabel?: boolean
+}
+
+const Slider: React.FC<Props> = ({
+  className = '',
+  tooltipPlacement = 'bottom',
+  withLabel = false,
+  ...rest
+}): JSX.Element => {
+  return (
+    <>
+      <SliderWrapper className={`slider-wrapper ${className}`} tooltipPlacement={tooltipPlacement} {...rest} />
+      {withLabel && (
+        <SliderLabel>
+          <span>0%</span>
+          <span>100%</span>
+        </SliderLabel>
+      )}
+    </>
+  )
+}
+
+export default Slider
