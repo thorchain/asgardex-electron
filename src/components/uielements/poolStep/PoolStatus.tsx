814530bead0e3501adde80c5951872743a723199
Nick <nicholaszav@gmail.com>
2020-05-31 21:30:36 +0200

    Use Trend component in PoolStatus

--- src/components/uielements/poolStep/PoolStatus.tsx
+++ src/components/uielements/poolStep/PoolStatus.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { formatBN } from '@thorchain/asgardex-util'
 import { Row } from 'antd'
 import BigNumber from 'bignumber.js'
 import { useIntl } from 'react-intl'
 
 import Label from '../label'
+import Trend from '../trend'
 import { PoolStatusWrapper } from './PoolStatus.style'
 
 type Props = {
@@ -19,17 +19,13 @@ type Props = {
 const PoolStatus: React.FC<Props> = (props: Props): JSX.Element => {
   const { amount, asset, label, target, trend } = props
   const intl = useIntl()
-
-  const trendPositive = trend.isGreaterThanOrEqualTo(0)
-  const trendVal = `${trendPositive ? '+' : '-'} ${formatBN(trend)}%`
-  const trendColor = trendPositive ? 'error' : 'success'
   const amountVal = intl.formatNumber(amount.toNumber(), { currency: 'USD', style: 'currency' })
 
   return (
     <PoolStatusWrapper {...props}>
       <Row style={{ justifyContent: 'space-between' }}>
         <Label color="light">{asset + ' / ' + target}</Label>
-        <Label color={trendColor}>{trendVal}</Label>
+        <Trend amount={trend} />
       </Row>
       <Label size="big" className="amount">
         {amountVal}
