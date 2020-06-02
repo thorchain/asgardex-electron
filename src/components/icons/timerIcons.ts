50ac3ff8d6358fb5264b0cd2fdd0ab8de4f6de45
FrozenIce0617 <prosdev087@gmail.com>
2020-06-02 18:30:57 +0100

    Add Timer Icons

---
+++ src/components/icons/timerIcons.ts
@@ -0,0 +1,9 @@
+import { ReactComponent as ConfirmIcon } from '../../assets/timer/icon-tick-fill.svg'
+import { ReactComponent as RefundIcon } from '../../assets/timer/refund.svg'
+import { ReactComponent as TimerChartIcon1 } from '../../assets/timer/timerchart-1.svg'
+import { ReactComponent as TimerChartIcon2 } from '../../assets/timer/timerchart-2.svg'
+import { ReactComponent as TimerChartIcon3 } from '../../assets/timer/timerchart-3.svg'
+import { ReactComponent as TimerChartIcon4 } from '../../assets/timer/timerchart-4.svg'
+import { ReactComponent as TimerFullIcon } from '../../assets/timer/timerchart-full.svg'
+
+export { ConfirmIcon, RefundIcon, TimerChartIcon1, TimerChartIcon2, TimerChartIcon3, TimerChartIcon4, TimerFullIcon }
