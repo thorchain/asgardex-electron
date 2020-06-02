6bc893e28d2fd981fae2148fb1d5b88b1147c4eb
Nick <nicholaszav@gmail.com>
2020-06-02 21:39:55 +0200

    Add status component

---
+++ src/components/uielements/status/Status.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Status from './Status'
+
+storiesOf('Components/Status', module).add('default', () => {
+  return (
+    <div>
+      <Status title="pool" value="bnb:bolt" />
+      <Status loading />
+      <Status title="pool2" value="bnb:bolt" direction="horizontal" />
+      <Status loading direction="horizontal" />
+    </div>
+  )
+})
