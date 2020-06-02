f504b61a81b8774fd45ac21cb9a2b7e0dfe345fe
FrozenIce0617 <prosdev087@gmail.com>
2020-06-02 18:31:34 +0100

    Add TxTimer component

---
+++ src/components/uielements/txTimer/TxTimer.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { withKnobs, boolean, number } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/react'
+
+import TxTimer from './TxTimer'
+
+storiesOf('Components/TxTimer', module)
+  .addDecorator(withKnobs)
+  .add('dynamicValues', () => {
+    return (
+      <TxTimer
+        status={boolean('status', false)}
+        value={number('value', 0)}
+        maxValue={number('max value', 100)}
+        startTime={Date.now()}
+      />
+    )
+  })
