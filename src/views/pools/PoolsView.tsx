ebc24ff52877ff617ff43d728c50d5faf48b9626
Veado <veado@protonmail.com>
2020-06-03 16:31:29 +0200

    Update routes to new structure
    - `swap` + `stake` routes goes under `pools` now
    - Update tests for routes
    - Add empty `PoolsView`

---
+++ src/views/pools/PoolsView.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Route, Switch } from 'react-router-dom'
+
+import * as poolsRoutes from '../../routes/pools'
+import * as stakeRoutes from '../../routes/stake'
+import * as swapRoutes from '../../routes/swap'
+import StakeView from '../stake/StakeView'
+import SwapView from '../swap/SwapView'
+import PoolsOverview from './PoolsOverview'
+
+const PoolsView: React.FC = (): JSX.Element => {
+  return (
+    <Switch>
+      <Route path={poolsRoutes.base.template} exact>
+        <PoolsOverview />
+      </Route>
+      <Route path={swapRoutes.swap.template} exact>
+        <SwapView />
+      </Route>
+      <Route path={stakeRoutes.stake.template} exact>
+        <StakeView />
+      </Route>
+    </Switch>
+  )
+}
+
+export default PoolsView
