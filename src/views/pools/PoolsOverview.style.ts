722af81975ea7830df787872d27c97e8e1c9c975
Veado <veado@protonmail.com>
2020-06-09 19:22:19 +0200

    Styles for `TimerLabel`

--- src/views/pools/PoolsOverview.style.ts
+++ src/views/pools/PoolsOverview.style.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import Label from '../../components/uielements/label'
+
 export const TableAction = styled.div`
   display: flex;
   align-items: center;
@@ -13,4 +15,9 @@ export const ActionColumn = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`
+
+export const TimerLabel = styled(Label)`
+  display: inline-block;
+  width: 100px;
 `
