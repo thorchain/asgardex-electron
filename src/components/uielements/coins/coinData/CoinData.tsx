84c985327d65a872ceb496946cd841e1bb6674fd
FrozenIce0617 <prosdev087@gmail.com>
2020-06-10 03:34:54 +0100

    Add CoinData component

---
+++ src/components/uielements/coins/coinData/CoinData.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import { TokenAmount, formatTokenAmount } from '@thorchain/asgardex-token'
+import { bn, formatBN } from '@thorchain/asgardex-util'
+import BigNumber from 'bignumber.js'
+
+import Label from '../../label'
+import Coin from '../coin'
+import { CoinDataWrapper, CoinDataWrapperType, CoinDataWrapperSize } from './CoinData.style'
+
+type Props = {
+  'data-test'?: string
+  asset?: string
+  assetValue?: TokenAmount
+  target?: string
+  targetValue?: TokenAmount
+  price?: BigNumber
+  priceUnit?: string
+  priceValid?: boolean
+  size?: CoinDataWrapperSize
+  className?: string
+  type?: CoinDataWrapperType | undefined
+}
+
+const CoinData: React.FC<Props> = (props: Props): JSX.Element => {
+  const {
+    asset = 'bnb',
+    assetValue = null,
+    target = undefined,
+    targetValue = null,
+    price = bn(0),
+    priceUnit = 'RUNE',
+    priceValid = true,
+    size = 'small',
+    className = '',
+    type = 'normal',
+    ...otherProps
+  } = props
+
+  const priceLabel = priceValid ? `${priceUnit.toUpperCase()} ${formatBN(price)}` : 'NOT LISTED'
+
+  return (
+    <CoinDataWrapper
+      size={size}
+      target={target}
+      type={type}
+      className={`coinData-wrapper ${className}`}
+      {...otherProps}>
+      <Coin className="coinData-coin-avatar" type={asset} over={target} size={size} />
+      <div className="coinData-asset-info" data-test="coin-data-asset-info">
+        <Label className="coinData-asset-label" data-test="coin-data-asset-label" weight="600">
+          {`${asset} ${target ? ':' : ''}`}
+        </Label>
+        {assetValue && (
+          <Label className="coinData-asset-value" data-test="coin-data-asset-value" weight="600">
+            {formatTokenAmount(assetValue)}
+          </Label>
+        )}
+      </div>
+      {target && (
+        <div className="coinData-target-info">
+          <Label className="coinData-target-label" weight="600">
+            {target}
+          </Label>
+          {targetValue && (
+            <Label className="coinData-target-value" weight="600">
+              {formatTokenAmount(targetValue)}
+            </Label>
+          )}
+        </div>
+      )}
+      <div className="asset-price-info">
+        <Label size="small" color="gray" weight="bold">
+          {priceLabel}
+        </Label>
+      </div>
+    </CoinDataWrapper>
+  )
+}
+
+export default CoinData
