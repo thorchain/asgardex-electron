cf76390a86f7b56544895cd4a8a39438659834d9
Veado <veado@protonmail.com>
2020-06-17 08:56:39 +0200

    Fix horizontal scroll bar

--- src/views/View.style.tsx
+++ src/views/View.style.tsx
@@ -4,7 +4,6 @@ import { size } from 'styled-theme'
 
 export const ViewWrapper = styled(Layout.Content)`
   padding: 70px 50px;
-  width: 100vw;
   margin-top: ${size('headerHeight')};
   margin-bottom: ${size('footerHeight')};
   min-height: calc(100vh - ${size('headerHeight')} - ${size('footerHeight')});
