006ebac61268d2ab8aa1c8239ea5aa51677152c2
FrozenIce0617 <prosdev087@gmail.com>
2020-07-08 12:20:33 +0100

    Add i18n for Settings

---
+++ src/i18n/de/settings.ts
@@ -0,0 +1,16 @@
+import { SettingMessages } from '../types'
+
+const settings: SettingMessages = {
+  'setting.title': 'Settings - DE',
+  'setting.wallet.management': 'Wallet Management - DE',
+  'setting.client': 'Client - DE',
+  'setting.account.management': 'Account Management - DE',
+  'setting.export': 'Export Keystore - DE',
+  'setting.lock': 'Lock - DE',
+  'setting.view.phrase': 'View Phrase - DE',
+  'setting.remove.wallet': 'Remove Wallet - DE',
+  'setting.midgard': 'Midgard - DE',
+  'setting.version': 'Version - DE'
+}
+
+export default settings
