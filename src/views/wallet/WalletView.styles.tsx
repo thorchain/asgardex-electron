721844e1a65ddf567146e9d4d2c4f9fa5acca06e
veado <61792675+veado@users.noreply.github.com>
2020-07-09 18:46:16 +0200

    Fix style of refresh button (#256)
    by providing a "transparent" button

--- src/views/wallet/WalletView.styles.tsx
+++ src/views/wallet/WalletView.styles.tsx
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 import Button from '../../components/uielements/button'
 
 export const RefreshButton = styled(Button)`
-  border: none;
-  background: none !important;
   margin-bottom: 20px;
   box-shadow: none;
 `
