259237281b5d8ac7e91e2cba5aaa4305b077cd45
thatStrangeGuyThorchain <67793194+thatStrangeGuyThorchain@users.noreply.github.com>
2020-07-10 11:10:36 +0300

    removed animations from common ant elements (#258)

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -12,6 +12,21 @@ export const AppWrapper = styled.div<AppWrapperProps>`
   height: 100vh;
   background: ${palette('background', 3)};
   padding: ${(props) => (props?.space === 'wide' ? '10px' : '0')};
+
+  a {
+    transition: none;
+  }
+
+  .ant {
+    &-btn,
+    &-input,
+    &-menu,
+    &-input-affix-wrapper,
+    &-table-thead > tr > th,
+    &-table-tbody > tr > td {
+      transition: none;
+    }
+  }
 `
 
 export const AppLayout = styled(Layout)`
