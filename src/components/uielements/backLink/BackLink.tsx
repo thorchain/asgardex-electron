4693d9135f6ed5e0c3c24fcbcd739dabfa481047
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-07-13 22:22:01 +0300

    added hiding backlinks text on mobile small devices

--- src/components/uielements/backLink/BackLink.tsx
+++ src/components/uielements/backLink/BackLink.tsx
@@ -3,22 +3,23 @@ import React from 'react'
 import { LeftOutlined } from '@ant-design/icons'
 import { useHistory } from 'react-router-dom'
 
-import { BackLinkWrapper } from './BackLink.style'
+import { BackLinkWrapper, ButtonText } from './BackLink.style'
 
 type Props = {
   title?: string
+  style?: React.CSSProperties
 }
 
-const BackLink: React.FC<Props> = ({ title = 'Back' }): JSX.Element => {
+const BackLink: React.FC<Props> = ({ title = 'Back', style }): JSX.Element => {
   const history = useHistory()
 
   const clickHandler = () => {
     history.goBack()
   }
   return (
-    <BackLinkWrapper onClick={clickHandler}>
+    <BackLinkWrapper onClick={clickHandler} style={style}>
       <LeftOutlined />
-      <span>{title}</span>
+      <ButtonText>{title}</ButtonText>
     </BackLinkWrapper>
   )
 }
