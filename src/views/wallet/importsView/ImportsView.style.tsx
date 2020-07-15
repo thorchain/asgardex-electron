43ba68cd6c424f4ee5a1838b384e3510dae5a534
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-07-15 20:43:46 +0300

    removed useless styled components from ImportsView.style

--- src/views/wallet/importsView/ImportsView.style.tsx
+++ src/views/wallet/importsView/ImportsView.style.tsx
@@ -1,8 +1,5 @@
-import * as A from 'antd'
 import styled from 'styled-components'
 import { palette } from 'styled-theme'
-
-import Label from '../../../components/uielements/label'
 
 export const ImportsViewWrapper = styled.div`
   flex: 1;
@@ -21,22 +18,4 @@ export const ImportsViewWrapper = styled.div`
     height: 5px;
     background: ${palette('gradient', 0)};
   }
-`
-
-export const Tabs = styled(A.Tabs).attrs({
-  size: 'large'
-})`
-  padding-top: 20px;
-  background-color: ${palette('background', 1)};
-`
-
-export const TabLabel = styled(Label)`
-  color: ${palette('black', 1)};
-  padding: 0;
-  padding-left: 20px;
-  padding-right: 20px;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 19px;
-  text-transform: uppercase;
 `
