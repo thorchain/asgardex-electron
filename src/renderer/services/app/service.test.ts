24da9111dac236ea14bed21c1a383fde9490dbd0
Veado <veado@protonmail.com>
2020-07-21 20:22:06 +0200

    Test for `toggleNetwork()` before subscribing

--- src/renderer/services/app/service.test.ts
+++ src/renderer/services/app/service.test.ts
@@ -13,6 +13,12 @@ describe('services/app/service/', () => {
       expect(network$).toBeObservable(expected)
     })
 
+    it('returns mainnet by toggleNetwork() before', () => {
+      toggleNetwork()
+      const expected = cold('a', { a: Network.MAIN })
+      expect(network$).toBeObservable(expected)
+    })
+
     it('toggleNetwork() changes network four times', () => {
       const toggle = '--a----a--a--a'
       const result = '--a----b--a--b'
@@ -21,7 +27,7 @@ describe('services/app/service/', () => {
         withLatestFrom(network$),
         map(([_, network]) => network)
       )
-      const expected = cold(result, { a: Network.MAIN, b: Network.TEST })
+      const expected = cold(result, { a: Network.TEST, b: Network.MAIN })
       expect(result$).toBeObservable(expected)
     })
   })
