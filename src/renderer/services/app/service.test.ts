8d3607adfdeb727935427ab907abd0838d11137d
Veado <veado@protonmail.com>
2020-07-21 20:18:37 +0200

    Add tests for `network$` + `toggleNetwork`

---
+++ src/renderer/services/app/service.test.ts
@@ -0,0 +1,28 @@
+import { cold } from 'jest-marbles'
+import { tap, map, withLatestFrom } from 'rxjs/operators'
+
+import { network$, toggleNetwork } from './service'
+import { Network } from './types'
+
+describe('services/app/service/', () => {
+  beforeEach(() => {})
+
+  describe('network$', () => {
+    it('returns testnet by default', () => {
+      const expected = cold('a', { a: Network.TEST })
+      expect(network$).toBeObservable(expected)
+    })
+
+    it('toggleNetwork() changes network four times', () => {
+      const toggle = '--a----a--a--a'
+      const result = '--a----b--a--b'
+      const toggle$ = cold(toggle, { a: 1 }).pipe(tap(() => toggleNetwork()))
+      const result$ = toggle$.pipe(
+        withLatestFrom(network$),
+        map(([_, network]) => network)
+      )
+      const expected = cold(result, { a: Network.MAIN, b: Network.TEST })
+      expect(result$).toBeObservable(expected)
+    })
+  })
+})
