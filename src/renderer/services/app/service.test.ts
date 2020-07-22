53453992f057f70eac2259dbe60c9688a1fb68b9
Veado <veado@protonmail.com>
2020-07-22 10:01:14 +0200

    Test onlineStatus$

--- src/renderer/services/app/service.test.ts
+++ src/renderer/services/app/service.test.ts
@@ -1,8 +1,8 @@
 import { cold } from 'jest-marbles'
 import { tap, map, withLatestFrom } from 'rxjs/operators'
 
-import { network$, toggleNetwork } from './service'
-import { Network } from './types'
+import { network$, toggleNetwork, onlineStatus$ } from './service'
+import { Network, OnlineStatus } from './types'
 
 describe('services/app/service/', () => {
   describe('network$', () => {
@@ -27,6 +27,30 @@ describe('services/app/service/', () => {
         tap(() => toggleNetwork()),
         withLatestFrom(network$),
         map(([_, network]) => network)
+      )
+      expect(result$).toBeObservable(expected)
+    })
+  })
+
+  describe('onlineStatus$', () => {
+    it('changes status different times', () => {
+      const online = () => window.dispatchEvent(new Event('online'))
+      const offline = () => window.dispatchEvent(new Event('offline'))
+
+      const values = { a: 'on', b: 'off', c: OnlineStatus.ON, d: OnlineStatus.OFF }
+      const toggle = '--a--b--a--b'
+      const result = '--c--d--c--d'
+
+      const changeOnlineStatus = (value: string) => {
+        if (value === 'on') online()
+        if (value === 'off') offline()
+        return true
+      }
+      const expected = cold(result, values)
+      const result$ = cold(toggle, values).pipe(
+        map(changeOnlineStatus),
+        withLatestFrom(onlineStatus$),
+        map(([_, status]) => status)
       )
       expect(result$).toBeObservable(expected)
     })
