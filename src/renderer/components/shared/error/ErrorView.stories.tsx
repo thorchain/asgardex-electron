a3e679d85fbfceb87a0cfe7848c9a85a7803c2f4
Veado <veado@protonmail.com>
2020-08-07 16:32:18 +0200

    Add action button to error view

--- src/renderer/components/shared/error/ErrorView.stories.tsx
+++ src/renderer/components/shared/error/ErrorView.stories.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { SyncOutlined } from '@ant-design/icons'
 import { storiesOf } from '@storybook/react'
 
+import Button from '../../uielements/button'
 import ErrorView from './ErrorView'
 
-storiesOf('Components/ErrorView', module).add('default', () => {
-  return <ErrorView message="Error while loading data!" />
-})
+storiesOf('Components/ErrorView', module)
+  .add('default', () => {
+    return <ErrorView message="Error while loading data!" />
+  })
+  .add('action button', () => {
+    const renderActionButton = () => (
+      <Button onClick={() => console.log('action')} typevalue="outline">
+        <SyncOutlined />
+        Action Button
+      </Button>
+    )
+    return <ErrorView message="Error while loading data!" actionButton={renderActionButton()} />
+  })
