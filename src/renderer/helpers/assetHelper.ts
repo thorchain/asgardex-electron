5061072515b20ecc2d0a0beeaf06cc8b1e2ce275
Veado <veado@protonmail.com>
2020-08-12 09:44:26 +0200

    `isRune()` helper (incl. tests)

---
+++ src/renderer/helpers/assetHelper.ts
@@ -0,0 +1,12 @@
+import { Asset } from '@thorchain/asgardex-util'
+
+// Note: All of following content will be go into `@thorchain/asgardex-util`
+
+export const RUNE_SYMBOL_MAINNET = 'RUNE-B1A'
+export const RUNE_SYMBOL_TESTNET = 'RUNE-67C'
+
+/**
+ * Check whether is RUNE asset or not
+ */
+export const isRuneAsset = ({ symbol }: Asset): boolean =>
+  symbol === RUNE_SYMBOL_MAINNET || symbol === RUNE_SYMBOL_TESTNET
