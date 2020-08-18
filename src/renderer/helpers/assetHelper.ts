07ca3c601d5730754f9a62fd59cd7e849ea3e81b
Veado <veado@protonmail.com>
2020-08-18 12:25:09 +0200

    Add `assetHelper`

---
+++ src/renderer/helpers/assetHelper.ts
@@ -0,0 +1,20 @@
+import { Asset, EMPTY_ASSET, assetAmount } from '@thorchain/asgardex-util'
+
+import { AssetWithBalance } from '../services/binance/types'
+
+export const EMTPY_ASSET_WITH_BALANCE: AssetWithBalance = {
+  asset: EMPTY_ASSET,
+  balance: assetAmount(0),
+  frozenBalance: assetAmount(0)
+}
+
+// Note: All of following content will be go into `@thorchain/asgardex-util`
+
+export const RUNE_SYMBOL_MAINNET = 'RUNE-B1A'
+export const RUNE_SYMBOL_TESTNET = 'RUNE-67C'
+
+/**
+ * Check whether is RUNE asset or not
+ */
+export const isRuneAsset = ({ symbol }: Asset): boolean =>
+  symbol === RUNE_SYMBOL_MAINNET || symbol === RUNE_SYMBOL_TESTNET
