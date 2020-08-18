a12141a0af1b2f9d82bb8458736bfd1994a14f5e
veado <61792675+veado@users.noreply.github.com>
2020-08-18 11:53:43 +0200

    Add `SuccessView` (#358)

---
+++ src/renderer/components/shared/success/SuccessView.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import Button from '../../uielements/button'
+import SuccessView from './SuccessView'
+
+storiesOf('Components/SuccessView', module)
+  .add('default', () => {
+    return <SuccessView title="Data loaded successfully!" />
+  })
+  .add('action button', () => {
+    const renderActionButton = () => (
+      <Button onClick={() => console.log('action')} typevalue="outline">
+        Click me
+      </Button>
+    )
+    return <SuccessView title="Data loaded successfully!" extra={renderActionButton()} />
+  })
---
+++ src/renderer/components/shared/success/SuccessView.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { ResultProps } from 'antd/lib/result'
+
+import * as Styled from './SuccessView.style'
+
+type Props = Omit<ResultProps, 'icon'>
+
+const SuccessView: React.FC<Props> = (props: Props): JSX.Element => (
+  <Styled.Result
+    icon={
+      <Styled.IconWrapper>
+        <Styled.Icon />
+      </Styled.IconWrapper>
+    }
+    {...props}
+  />
+)
+
+export default SuccessView
