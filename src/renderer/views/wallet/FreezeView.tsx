ee058d8a8372069864bc4c9df9cf892120a7796b
Veado <veado@protonmail.com>
2020-08-25 11:03:01 +0200

    Don't render error view - which causes flickering

--- src/renderer/views/wallet/FreezeView.tsx
+++ src/renderer/views/wallet/FreezeView.tsx
@@ -9,7 +9,6 @@ import { useObservableState } from 'observable-hooks'
 import { useIntl } from 'react-intl'
 import { useParams } from 'react-router'
 
-import ErrorView from '../../components/shared/error/ErrorView'
 import BackLink from '../../components/uielements/backLink'
 import Freeze from '../../components/wallet/txs/Freeze'
 import { useBinanceContext } from '../../contexts/BinanceContext'
@@ -49,12 +48,7 @@ const FreezeView: React.FC<Props> = ({ freezeAction }): JSX.Element => {
       {FP.pipe(
         oSelectedAssetWB,
         O.fold(
-          () => (
-            <>
-              <BackLink />
-              <ErrorView title={`Parsing asset ${asset} from route failed`} />
-            </>
-          ),
+          () => <></>,
           (selectedAsset) => (
             <>
               <BackLink path={walletRoutes.assetDetail.path({ asset: assetToString(selectedAsset.asset) })} />
