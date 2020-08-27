b66af5c02261495bd203f3e02d0c9b17eeb169a3
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-08-27 13:09:29 +0300

    added stories for AssetMenu

--- src/renderer/components/uielements/assets/assetMenu/AssetMenu.stories.tsx
+++ src/renderer/components/uielements/assets/assetMenu/AssetMenu.stories.tsx
@@ -7,14 +7,56 @@ import { ASSETS_MAINNET } from '../../../../../shared/mock/assets'
 import { ONE_ASSET_BASE_AMOUNT } from '../../../../const'
 import AssetMenu from './AssetMenu'
 
-storiesOf('Components/Assets/AssetMenu', module).add('default', () => {
+storiesOf('Components/Assets/AssetMenu', module).add('with search', () => {
+  return (
+    <div style={{ display: 'flex', padding: '20px' }}>
+      <AssetMenu
+        withSearch={true}
+        asset={ASSETS_MAINNET.BNB}
+        assetData={[
+          {
+            asset: ASSETS_MAINNET.RUNE,
+            price: ONE_ASSET_BASE_AMOUNT
+          },
+          {
+            asset: ASSETS_MAINNET.BNB,
+            price: ONE_ASSET_BASE_AMOUNT
+          },
+          {
+            asset: ASSETS_MAINNET.FTM,
+            price: ONE_ASSET_BASE_AMOUNT
+          },
+          {
+            asset: ASSETS_MAINNET.TOMO,
+            price: ONE_ASSET_BASE_AMOUNT
+          }
+        ]}
+        priceIndex={{
+          RUNE: bn(1)
+        }}
+        searchDisable={[]}
+        onSelect={(key) => console.log(key)}
+      />
+    </div>
+  )
+})
+
+storiesOf('Components/Assets/AssetMenu', module).add('without search', () => {
   return (
     <div style={{ display: 'flex', padding: '20px' }}>
       <AssetMenu
         asset={ASSETS_MAINNET.BNB}
         assetData={[
           {
+            asset: ASSETS_MAINNET.RUNE,
+            price: ONE_ASSET_BASE_AMOUNT
+          },
+          {
             asset: ASSETS_MAINNET.BNB,
+            price: ONE_ASSET_BASE_AMOUNT
+          },
+          {
+            asset: ASSETS_MAINNET.FTM,
             price: ONE_ASSET_BASE_AMOUNT
           },
           {
