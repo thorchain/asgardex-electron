2f77b122adaa84f4b25a17d0f36f7f297a8aa6b0
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-09-11 16:21:14 +0300

    stake skeleton added

---
+++ src/renderer/components/stake/Stake.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import { Stake } from './Stake'
+
+storiesOf('Stake', module).add('default', () => {
+  return <Stake />
+})
