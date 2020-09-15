143d5108b55a71909878de8fe29a2a69e22c83c9
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-09-15 12:28:33 +0300

    added stake DE ytanslations

--- src/renderer/i18n/de/stake.ts
+++ src/renderer/i18n/de/stake.ts
@@ -1,9 +1,9 @@
 import { StakeMessages } from '../types'
 
 const stake: StakeMessages = {
-  'stake.totalShare': 'Your total share of the pool - DE',
-  'stake.totalEarnings': 'Your total earnings from the pool - DE',
-  'stake.poolShare': ' Pool share - DE'
+  'stake.totalShare': 'Dein Poolanteil',
+  'stake.totalEarnings': 'Deine Gesamteinkommen vom Pool',
+  'stake.poolShare': ' Poolanteil'
 }
 
 export default stake
