4362424678b5e45ceca74af4ecf1b5af8ba8389a
Veado <veado@protonmail.com>
2020-09-30 17:52:53 +0200

    Fix i18n typo

--- src/renderer/i18n/de/swap.ts
+++ src/renderer/i18n/de/swap.ts
@@ -2,7 +2,7 @@ import { SwapMessages } from '../types'
 
 const swap: SwapMessages = {
   'swap.swapping': 'Tauschen',
-  'swap.state.pending': 'Tauschen',
+  'swap.state.pending': 'Tauschvorgang läuft',
   'swap.state.success': 'Erfolgreich getauscht',
   'swap.viewTransaction': 'Transaktion ansehen',
   'swap.input': 'Eingabe',
