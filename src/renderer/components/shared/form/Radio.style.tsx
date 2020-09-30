d2fe55c836643306721cd89f0ce0fb4b2af47960
Veado <veado@protonmail.com>
2020-09-30 17:17:35 +0200

    Style antd `Radio` component

---
+++ src/renderer/components/shared/form/Radio.style.tsx
@@ -0,0 +1,33 @@
+import * as A from 'antd'
+import styled from 'styled-components'
+import { palette } from 'styled-theme'
+
+import UILabel from '../../uielements/label'
+
+export const Radio = styled(A.Radio)`
+  > .ant-radio .ant-radio-inner {
+    border-color: ${palette('primary', 0)};
+    background-color: ${palette('background', 0)};
+  }
+
+  > .ant-radio:hover .ant-radio-inner {
+    border-color: ${palette('primary', 0)};
+  }
+
+  > .ant-radio-checked .ant-radio-inner {
+    border-color: ${palette('primary', 0)};
+
+    &::after {
+      background-color: ${palette('primary', 0)};
+    }
+  }
+`
+
+export const RadioLabel = styled(UILabel).attrs({
+  textTransform: 'uppercase',
+  size: 'normal'
+})`
+  display: inline-block;
+  padding: 0;
+  margin: 0;
+`
