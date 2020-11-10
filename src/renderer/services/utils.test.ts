5c7d13fb63ce95350f2b9f49b6a36d2d89ddd983
thatStrangeGuyThorchain <67793194+thatStrangeGuyThorchain@users.noreply.github.com>
2020-11-10 20:39:00 +0300

    Remove usage of '@thorchain/asgardex-binance' #587
    removed left thorchain-binance usages

--- src/renderer/services/utils.test.ts
+++ src/renderer/services/utils.test.ts
@@ -1,4 +1,4 @@
-import { BinanceClient } from '@thorchain/asgardex-binance'
+import { Client } from '@xchainjs/xchain-binance'
 import * as E from 'fp-ts/lib/Either'
 import * as O from 'fp-ts/lib/Option'
 
@@ -7,9 +7,9 @@ import { getClient, hasClient, getClientStateForViews } from './utils'
 
 // Mocking non default class exports
 // https://jestjs.io/docs/en/es6-class-mocks#mocking-non-default-class-exports
-jest.mock('@thorchain/asgardex-binance', () => {
+jest.mock('@xchainjs/xchain-binance', () => {
   return {
-    BinanceClient: jest.fn().mockImplementation(() =>
+    Client: jest.fn().mockImplementation(() =>
       /* return empty object - we don't need mock any functions in tests here */
       {}
     )
@@ -17,15 +17,14 @@ jest.mock('@thorchain/asgardex-binance', () => {
 })
 
 describe('services/utils/', () => {
-  let mockClient: BinanceClient
+  let mockClient: Client
   beforeEach(() => {
-    BinanceClient.mockClear()
-    mockClient = new BinanceClient()
+    mockClient = new Client({})
   })
 
   describe('getClient (Binance)', () => {
     it('returns a client if it has been created before', () => {
-      const state: ClientState<BinanceClient> = O.some(E.right(mockClient))
+      const state: ClientState<Client> = O.some(E.right(mockClient))
       const result = getClient(state)
       expect(O.toNullable(result)).toEqual(mockClient)
     })
@@ -34,24 +33,24 @@ describe('services/utils/', () => {
       expect(result).toBeNone()
     })
     it('returns none if creating a client has throw an error before', () => {
-      const state: ClientState<BinanceClient> = O.some(E.left(new Error('any error')))
-      const result = getClient<BinanceClient>(state)
+      const state: ClientState<Client> = O.some(E.left(new Error('any error')))
+      const result = getClient<Client>(state)
       expect(result).toBeNone()
     })
   })
 
   describe('hasClient (Binance)', () => {
     it('returns true if a client has been created', () => {
-      const state: ClientState<BinanceClient> = O.some(E.right(mockClient))
+      const state: ClientState<Client> = O.some(E.right(mockClient))
       const result = hasClient(state)
       expect(result).toBeTruthy()
     })
     it('returns false if no client has been created', () => {
-      const result = hasClient<BinanceClient>(O.none)
+      const result = hasClient<Client>(O.none)
       expect(result).toBeFalsy()
     })
     it('returns false if any errors occur', () => {
-      const state: ClientState<BinanceClient> = O.some(E.left(new Error('any error')))
+      const state: ClientState<Client> = O.some(E.left(new Error('any error')))
       const result = hasClient(state)
       expect(result).toBeFalsy()
     })
@@ -59,16 +58,16 @@ describe('services/utils/', () => {
 
   describe('getClientStateForViews (Binance)', () => {
     it('returns true if a client has been created', () => {
-      const state: ClientState<BinanceClient> = O.some(E.right(mockClient))
-      const result = getClientStateForViews<BinanceClient>(state)
+      const state: ClientState<Client> = O.some(E.right(mockClient))
+      const result = getClientStateForViews<Client>(state)
       expect(result).toEqual('ready')
     })
     it('returns false if no client has been created', () => {
-      const result = getClientStateForViews<BinanceClient>(O.none)
+      const result = getClientStateForViews<Client>(O.none)
       expect(result).toEqual('notready')
     })
     it('returns false if any errors occur', () => {
-      const state: ClientState<BinanceClient> = O.some(E.left(new Error('any error')))
+      const state: ClientState<Client> = O.some(E.left(new Error('any error')))
       const result = getClientStateForViews(state)
       expect(result).toEqual('error')
     })
