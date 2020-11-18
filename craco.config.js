423a45194d6b1e2dbde0fcbb00831bfbab3f9e74
Veado <veado@protonmail.com>
2020-11-18 21:43:25 +0100

    Fix: craco:  *** Cannot find ESLint loader (eslint-loader). ***
    as described in https://github.com/gsoft-inc/craco/issues/205#issuecomment-716631300

--- craco.config.js
+++ craco.config.js
@@ -30,5 +30,26 @@ module.exports = {
         $IS_DEV: true
       }
     }
-  }
+  },
+  plugins: [
+    {
+      plugin: {
+        overrideCracoConfig: ({ cracoConfig }) => {
+          if (typeof cracoConfig.eslint.enable !== 'undefined') {
+            cracoConfig.disableEslint = !cracoConfig.eslint.enable
+          }
+          delete cracoConfig.eslint
+          return cracoConfig
+        },
+        overrideWebpackConfig: ({ webpackConfig, cracoConfig }) => {
+          if (typeof cracoConfig.disableEslint !== 'undefined' && cracoConfig.disableEslint === true) {
+            webpackConfig.plugins = webpackConfig.plugins.filter(
+              (instance) => instance.constructor.name !== 'ESLintWebpackPlugin'
+            )
+          }
+          return webpackConfig
+        }
+      }
+    }
+  ]
 }
