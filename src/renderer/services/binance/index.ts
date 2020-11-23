a5098803a70240e09818f7924eeb300168e72928
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-11-23 22:16:54 +0300

    removed wsTransfer$ from binance service

--- src/renderer/services/binance/index.ts
+++ src/renderer/services/binance/index.ts
@@ -2,7 +2,7 @@ import { balances$ } from './balances'
 import { client$, clientViewState$, address$, reloadBalances, explorerUrl$, getExplorerTxUrl$ } from './common'
 import { createFeesService } from './fees'
 import { createTransactionService } from './transaction'
-import { subscribeTransfers, miniTickers$, wsTransfer$ } from './ws'
+import { subscribeTransfers, miniTickers$ } from './ws'
 
 const { txs$, pushTx, resetTx, txRD$, sendStakeTx } = createTransactionService(client$)
 const { reloadFees, fees$ } = createFeesService(client$)
@@ -16,7 +16,6 @@ export {
   getExplorerTxUrl$,
   subscribeTransfers,
   miniTickers$,
-  wsTransfer$,
   balances$,
   txs$,
   pushTx,
