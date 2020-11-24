d189b7af0e0b3c216a238c4d824689325f46b1fa
Veado <veado@protonmail.com>
2020-11-24 14:21:25 +0100

    `fs.unlinkSync`

--- fix.sr25519.js
+++ fix.sr25519.js
@@ -5,6 +5,6 @@ const FILE_LIB_PATH = path.resolve(__dirname, 'node_modules/sr25519/sr25519.js')
 const FILE_FIX_PATH = path.resolve(__dirname, 'sr25519.js')
 
 if (fs.existsSync(FILE_LIB_PATH)) {
-  fs.rmSync(FILE_LIB_PATH)
+  fs.unlinkSync(FILE_LIB_PATH)
   fs.copyFileSync(FILE_FIX_PATH, FILE_LIB_PATH)
 }
