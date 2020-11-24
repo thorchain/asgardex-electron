d61b769c2934479f21ba07a7dbea6fc89e3ee552
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2020-11-24 16:08:11 +0300

    added fix script for sr25519

---
+++ fix.sr25519.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+const FILE_LIB_PATH = path.resolve(__dirname, 'node_modules/sr25519/sr25519.js')
+const FILE_FIX_PATH = path.resolve(__dirname, 'sr25519.js')
+
+if (fs.existsSync(FILE_LIB_PATH)) {
+  fs.rmSync(FILE_LIB_PATH)
+  fs.copyFileSync(FILE_FIX_PATH, FILE_LIB_PATH)
+}
