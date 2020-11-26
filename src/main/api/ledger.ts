5eb2bc3f6a742c944b938cdcd6b8a21ce590b7a5
sarawut <sanit.sa@outlook.com>
2020-11-27 02:27:31 +1000

    add error handler for getBTCAddress

--- src/main/api/ledger.ts
+++ src/main/api/ledger.ts
@@ -2,9 +2,13 @@ import AppBtc from '@ledgerhq/hw-app-btc'
 import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
 
 export const getBTCAddress = async () => {
-  const transport = await TransportNodeHid.open('')
-  const appBtc = new AppBtc(transport)
-  const info = await appBtc.getWalletPublicKey("44'/0'/0'/0/0")
-  await transport.close()
-  return info.bitcoinAddress
+  try {
+    const transport = await TransportNodeHid.open('')
+    const appBtc = new AppBtc(transport)
+    const info = await appBtc.getWalletPublicKey("44'/0'/0'/0/0")
+    await transport.close()
+    return info.bitcoinAddress
+  } catch (e) {
+    return Promise.reject(e)
+  }
 }
