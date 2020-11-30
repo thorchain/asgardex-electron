cdd3b2f5576c374ccab8e2ab45238d6d3497293c
sarawut <sanit.sa@outlook.com>
2020-12-01 03:46:44 +1000

    bug fix

--- src/main/api/hdwallet.ts
+++ src/main/api/hdwallet.ts
@@ -1,6 +1,6 @@
-import { Network } from '@xchainjs/xchain-client'
 import { ipcRenderer } from 'electron'
 
+import { Network } from '../../shared/api/types'
 import { ApiHDWallet } from '../../shared/api/types'
 import IPCMessages from '../ipc/messages'
 
