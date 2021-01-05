thorchain/asgardex-electron

commit 9caad93262f2cb961a04b5c964ddb6ead9058ecf

Add `PrivateModal` component (#700)
to deprecate `ConfirmPasswordView`.

- [x] Extract `ConfirmPasswordView` → `PasswordModal`, but without dependency of `WalletContext` 
- [x] Add stories `PasswordModal`
- [x] Update stories of `PrivateModal`
- [x] Update `Swap` + `AssetDetails` to use PrivateModel` (instead passing `ConfirmPasswordView`) 
- [x] Introduce types of `ValidatePasswordLD` + `ValidatePasswordHandler`

src/renderer/components/modal/password/PasswordModal.tsx
@@ -5,25 +5,22 @@ import * as FP from 'fp-ts/function'
 import { useObservableState } from 'observable-hooks'
 
 import { PrivateModal } from '../../../components/modal/private'
-import { ConfirmationModalProps } from '../../../components/uielements/common/Common.types'
-import { useWalletContext } from '../../../contexts/WalletContext'
+import { ValidatePasswordLD } from '../../../services/wallet/types'
 
 /**
- * @Note
- * This is not a 'usual' view
- * Just a modal 'connected' to the all appropriate services
+ * Wrapper around `PrivateModal` to validate password using `validatePassword$` stream
  */
 
-export const ConfirmPasswordView: React.FC<ConfirmationModalProps> = ({ onSuccess, onClose }) => {
+export type Props = {
+  onSuccess: FP.Lazy<void>
+  onClose: FP.Lazy<void>
+  validatePassword$: (_: string) => ValidatePasswordLD
+}
+
+export const PasswordModal: React.FC<Props> = ({ onSuccess, onClose, validatePassword$ }) => {
   const [passwordToValidate, setPasswordToValidate] = useState('')
 
-  const { keystoreService } = useWalletContext()
-  const passwordValidationResult$ = useMemo(() => keystoreService.validatePassword$(passwordToValidate), [
-    passwordToValidate,
-    keystoreService
-  ])
-
-  const passwordValidationResult = useObservableState(passwordValidationResult$, RD.initial)
+  const [passwordValidationRD] = useObservableState(() => validatePassword$(passwordToValidate), RD.initial)
 
   const closePrivateModal = useCallback(() => {
     onClose()
@@ -38,7 +35,7 @@ export const ConfirmPasswordView: React.FC<ConfirmationModalProps> = ({ onSucces
   const confirmProps = useMemo(() => {
     const props = { onCancel: closePrivateModal, visible: true }
     return FP.pipe(
-      passwordValidationResult,
+      passwordValidationRD,
       RD.fold(
         () => ({
           ...props,
@@ -62,7 +59,7 @@ export const ConfirmPasswordView: React.FC<ConfirmationModalProps> = ({ onSucces
         })
       )
     )
-  }, [passwordValidationResult, setPasswordToValidate, closePrivateModal, onPasswordValidationSucceed])
+  }, [passwordValidationRD, setPasswordToValidate, closePrivateModal, onPasswordValidationSucceed])
 
   return <PrivateModal {...confirmProps} />
 }

src/renderer/components/modal/password/PasswordModal.stories.tsx
@@ -1,0 +1,55 @@
+import React from 'react'
+
+import * as RD from '@devexperts/remote-data-ts'
+import { Story, Meta } from '@storybook/react'
+import * as Rx from 'rxjs'
+
+import { ValidatePasswordHandler } from '../../../services/wallet/types'
+import { PasswordModal } from './PasswordModal'
+
+const onClose = () => console.log('onClose')
+const onSuccess = () => console.log('onCSuccess')
+const validatePasswordInitial$: ValidatePasswordHandler = (_) => Rx.of(RD.initial)
+const validatePasswordPending$: ValidatePasswordHandler = (_) => Rx.of(RD.pending)
+const validatePasswordFailure$: ValidatePasswordHandler = (_) => Rx.of(RD.failure(Error('invalid password')))
+const validatePasswordSuccess$: ValidatePasswordHandler = (_) => Rx.of(RD.success(undefined))
+
+export const StoryInitial: Story = () => (
+  <PasswordModal onClose={onClose} onSuccess={onSuccess} validatePassword$={validatePasswordInitial$} />
+)
+StoryInitial.storyName = 'initial'
+
+export const StorySuccess: Story = () => (
+  <PasswordModal onClose={onClose} onSuccess={onSuccess} validatePassword$={validatePasswordSuccess$} />
+)
+StorySuccess.storyName = 'success'
+
+export const StoryValidating: Story = () => (
+  <PasswordModal onClose={onClose} onSuccess={onSuccess} validatePassword$={validatePasswordPending$} />
+)
+StoryValidating.storyName = 'validating'
+
+export const StoryInvalid: Story = () => (
+  <PasswordModal onClose={onClose} onSuccess={onSuccess} validatePassword$={validatePasswordFailure$} />
+)
+
+StoryInvalid.storyName = 'invalid'
+
+const meta: Meta = {
+  component: PasswordModal,
+  title: 'Components/Password Modal',
+  decorators: [
+    (S: Story) => (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '300px'
+        }}>
+        <S />
+      </div>
+    )
+  ]
+}
+
+export default meta

src/renderer/components/modal/password/index.ts
@@ -1,0 +1,1 @@
+export { PasswordModal } from './PasswordModal'

src/renderer/components/modal/private/PrivateModal.stories.tsx
@@ -1,43 +1,33 @@
 import React from 'react'
 
-import { storiesOf } from '@storybook/react'
+import { Story, Meta } from '@storybook/react'
 
 import { PrivateModal } from './PrivateModal'
 
-storiesOf('Components/Private Modal', module)
-  .add('default', () => {
-    return (
+export const StoryDefault: Story = () => <PrivateModal visible invalidPassword validatingPassword={false} />
+StoryDefault.storyName = 'default'
+
+export const StoryInvalid: Story = () => <PrivateModal visible invalidPassword validatingPassword={false} />
+StoryInvalid.storyName = 'invalid'
+
+export const StoryValidating: Story = () => <PrivateModal visible invalidPassword={false} validatingPassword />
+StoryValidating.storyName = 'validating'
+
+const meta: Meta = {
+  component: PrivateModal,
+  title: 'Components/Private Modal',
+  decorators: [
+    (S: Story) => (
       <div
         style={{
           display: 'flex',
           flexDirection: 'column',
           width: '300px'
         }}>
-        <PrivateModal visible invalidPassword={false} validatingPassword={false} />
+        <S />
       </div>
     )
-  })
-  .add('invalid', () => {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '300px'
-        }}>
-        <PrivateModal visible invalidPassword validatingPassword={false} />
-      </div>
-    )
-  })
-  .add('validating', () => {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '300px'
-        }}>
-        <PrivateModal visible invalidPassword={false} validatingPassword />
-      </div>
-    )
-  })
+  ]
+}
+
+export default meta

src/renderer/components/modal/private/PrivateModal.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 
 import { Form } from 'antd'
+import * as FP from 'fp-ts/lib/function'
 import { useIntl } from 'react-intl'
 
 import { Input } from '../../uielements/input'
@@ -12,20 +13,29 @@ type Props = {
   invalidPassword?: boolean
   validatingPassword?: boolean
   onConfirm?: (password: string) => void
-  onOk?: () => void
-  onCancel?: () => void
+  onOk?: FP.Lazy<void>
+  onCancel?: FP.Lazy<void>
   isSuccess?: boolean
 }
 
 export const PrivateModal: React.FC<Props> = (props): JSX.Element => {
-  const { visible, invalidPassword, validatingPassword, onConfirm, onOk, onCancel, isSuccess } = props
+  const {
+    visible,
+    invalidPassword,
+    validatingPassword,
+    onConfirm = FP.constVoid,
+    onOk = FP.constVoid,
+    onCancel = FP.constVoid,
+    isSuccess
+  } = props
+
   const intl = useIntl()
 
   /**
    * Call onOk on success only
    */
   useEffect(() => {
-    if (isSuccess && onOk) {
+    if (isSuccess) {
       onOk()
     }
   }, [isSuccess, onOk])
@@ -40,8 +50,9 @@ export const PrivateModal: React.FC<Props> = (props): JSX.Element => {
   )
 
   const onConfirmCb = useCallback(() => {
-    onConfirm && onConfirm(password)
+    onConfirm(password)
   }, [onConfirm, password])
+
   return (
     <Styled.Modal
       title={intl.formatMessage({ id: 'wallet.password.confirmation' })}

src/renderer/components/swap/Swap.tsx
@@ -34,13 +34,13 @@ import { AssetsWithPrice, AssetWithPrice, TxWithStateRD } from '../../services/b
 import { SwapFeesRD } from '../../services/chain/types'
 import { PoolDetails } from '../../services/midgard/types'
 import { getPoolDetailsHashMap } from '../../services/midgard/utils'
-import { NonEmptyWalletBalances } from '../../services/wallet/types'
+import { NonEmptyWalletBalances, ValidatePasswordHandler } from '../../services/wallet/types'
 import { TxStatus, TxTypes } from '../../types/asgardex'
 import { PricePool } from '../../views/pools/Pools.types'
 import { CurrencyInfo } from '../currency'
+import { PasswordModal } from '../modal/password'
 import { SwapModal } from '../modal/swap'
 import { AssetSelect } from '../uielements/assets/assetSelect'
-import { ConfirmationModalProps } from '../uielements/common/Common.types'
 import { Fees, UIFeesRD } from '../uielements/fees'
 import { Modal } from '../uielements/modal'
 import { Slider } from '../uielements/slider'
@@ -59,7 +59,7 @@ type SwapProps = {
   resetTx?: () => void
   goToTransaction?: (txHash: string) => void
   activePricePool: PricePool
-  PasswordConfirmation: React.FC<ConfirmationModalProps>
+  validatePassword$: ValidatePasswordHandler
   reloadFees?: () => void
   fees?: SwapFeesRD
   targetWalletAddress?: O.Option<Address>
@@ -76,7 +76,7 @@ export const Swap = ({
   goToTransaction = (_) => {},
   resetTx,
   activePricePool,
-  PasswordConfirmation,
+  validatePassword$,
   reloadFees,
   fees: feesProp = RD.initial,
   targetWalletAddress = O.none
@@ -565,7 +565,11 @@ export const Swap = ({
   return (
     <Styled.Container>
       {showPrivateModal && (
-        <PasswordConfirmation onSuccess={onPasswordValidationSucceed} onClose={() => setShowPrivateModal(false)} />
+        <PasswordModal
+          onSuccess={onPasswordValidationSucceed}
+          onClose={() => setShowPrivateModal(false)}
+          validatePassword$={validatePassword$}
+        />
       )}
       <Styled.PendingContainer>{pendingState}</Styled.PendingContainer>
       <Styled.ContentContainer>

src/renderer/components/uielements/common/Common.types.ts
@@ -1,3 +1,0 @@
-import * as FP from 'fp-ts/lib/function'
-
-export type ConfirmationModalProps = { onSuccess: FP.Lazy<void>; onClose: FP.Lazy<void> }

src/renderer/components/wallet/assets/AssetDetails.stories.tsx
@@ -11,10 +11,8 @@ import * as RxOp from 'rxjs/operators'
 import { BNB_TRANSFER_FEES } from '../../../../shared/mock/fees'
 import { ZERO_BASE_AMOUNT } from '../../../const'
 import { SendTxParams } from '../../../services/binance/types'
-import { ErrorId, TxLD, TxRD } from '../../../services/wallet/types'
+import { ErrorId, TxLD, TxRD, ValidatePasswordHandler } from '../../../services/wallet/types'
 import { WalletBalance, WalletBalances } from '../../../types/wallet'
-import { PrivateModal } from '../../modal/private'
-import { ConfirmationModalProps } from '../../uielements/common/Common.types'
 import { AssetDetails } from './index'
 
 const bnbBalance: WalletBalance = {
@@ -67,12 +65,11 @@ const poolAddress = O.some('pool address')
 
 const thorAddress = O.some('thor address')
 
+const validatePasswordInitial$: ValidatePasswordHandler = (_) => Rx.of(RD.initial)
+const validatePasswordSuccess$: ValidatePasswordHandler = (_) => Rx.of(RD.success(undefined))
+
 const upgradeFee = RD.success(assetToBase(BNB_TRANSFER_FEES.single))
 const reloadUpgradeFeeHandler = () => console.log('reload upgrade fee')
-
-const UpgradeConfirmationModal: React.FC<ConfirmationModalProps> = ({ onSuccess, onClose }) => (
-  <PrivateModal visible onOk={onClose} onCancel={onClose} onConfirm={onSuccess} />
-)
 
 export const StoryBNB: BaseStory<never, JSX.Element> = () => (
   <AssetDetails
@@ -81,9 +78,9 @@ export const StoryBNB: BaseStory<never, JSX.Element> = () => (
     asset={O.some(AssetBNB)}
     sendUpgradeTx={sendUpgradeRuneTx}
     poolAddress={poolAddress}
+    validatePassword$={validatePasswordInitial$}
     upgradeFee={RD.initial}
     reloadUpgradeFeeHandler={reloadUpgradeFeeHandler}
-    UpgradeConfirmationModal={UpgradeConfirmationModal}
   />
 )
 StoryBNB.storyName = 'BNB'
@@ -96,9 +93,9 @@ export const StoryRuneTxSuccess: BaseStory<never, JSX.Element> = () => (
     sendUpgradeTx={sendUpgradeRuneTx}
     runeNativeAddress={thorAddress}
     poolAddress={poolAddress}
+    validatePassword$={validatePasswordSuccess$}
     upgradeFee={upgradeFee}
     reloadUpgradeFeeHandler={reloadUpgradeFeeHandler}
-    UpgradeConfirmationModal={UpgradeConfirmationModal}
   />
 )
 StoryRuneTxSuccess.storyName = 'RUNE - tx success'
@@ -111,9 +108,9 @@ export const StoryRuneTxError: BaseStory<never, JSX.Element> = () => (
     sendUpgradeTx={sendUpgradeRuneTxError}
     runeNativeAddress={thorAddress}
     poolAddress={poolAddress}
+    validatePassword$={validatePasswordSuccess$}
     upgradeFee={upgradeFee}
     reloadUpgradeFeeHandler={reloadUpgradeFeeHandler}
-    UpgradeConfirmationModal={UpgradeConfirmationModal}
   />
 )
 StoryRuneTxError.storyName = 'RUNE - tx error'
@@ -125,9 +122,9 @@ export const StoryRuneNoBalances: BaseStory<never, JSX.Element> = () => (
     asset={O.some(AssetRune67C)}
     sendUpgradeTx={sendUpgradeRuneTx}
     poolAddress={poolAddress}
+    validatePassword$={validatePasswordInitial$}
     upgradeFee={upgradeFee}
     reloadUpgradeFeeHandler={reloadUpgradeFeeHandler}
-    UpgradeConfirmationModal={UpgradeConfirmationModal}
   />
 )
 StoryRuneNoBalances.storyName = 'RUNE - disabled - no balance'
@@ -139,9 +136,9 @@ export const StoryRuneFeeNotCovered: BaseStory<never, JSX.Element> = () => (
     asset={O.some(AssetRune67C)}
     sendUpgradeTx={sendUpgradeRuneTx}
     poolAddress={poolAddress}
+    validatePassword$={validatePasswordInitial$}
     upgradeFee={upgradeFee}
     reloadUpgradeFeeHandler={reloadUpgradeFeeHandler}
-    UpgradeConfirmationModal={UpgradeConfirmationModal}
   />
 )
 StoryRuneFeeNotCovered.storyName = 'RUNE - fee not covered'

commit bec2e7c865bba27155289a2400107d55db01c8dc

Fix password validation in PasswordModal
This bug was introduced by #700 (especially by these changes
https://github.com/thorchain/asgardex-electron/pull/700/files#diff-9d1e356e47bf2d128553b374361af2ec167b58d670d81fdc4549843433bd6579L20-R23)

src/renderer/components/modal/password/PasswordModal.tsx
@@ -10,7 +10,6 @@ import { ValidatePasswordLD } from '../../../services/wallet/types'
 /**
  * Wrapper around `PrivateModal` to validate password using `validatePassword$` stream
  */
-
 export type Props = {
   onSuccess: FP.Lazy<void>
   onClose: FP.Lazy<void>
@@ -20,7 +19,12 @@ export type Props = {
 export const PasswordModal: React.FC<Props> = ({ onSuccess, onClose, validatePassword$ }) => {
   const [passwordToValidate, setPasswordToValidate] = useState('')
 
-  const [passwordValidationRD] = useObservableState(() => validatePassword$(passwordToValidate), RD.initial)
+  const passwordValidationResult$ = useMemo(() => validatePassword$(passwordToValidate), [
+    passwordToValidate,
+    validatePassword$
+  ])
+
+  const passwordValidationRD = useObservableState(passwordValidationResult$, RD.initial)
 
   const closePrivateModal = useCallback(() => {
     onClose()
@@ -61,5 +65,11 @@ export const PasswordModal: React.FC<Props> = ({ onSuccess, onClose, validatePas
     )
   }, [passwordValidationRD, setPasswordToValidate, closePrivateModal, onPasswordValidationSucceed])
 
-  return <PrivateModal {...confirmProps} />
+  return (
+    <>
+      <div> passwordToValidate: {JSON.stringify(passwordToValidate)}</div>
+      <div>passwordValidationRD: {JSON.stringify(passwordValidationRD)}</div>
+      <PrivateModal {...confirmProps} />
+    </>
+  )
 }