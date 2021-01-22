71a0a2fca4606747747ca5e27e83bb724f2704f6
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2021-01-22 17:43:19 +0300

    replaced formatBn with formatAssetAmountCurrency

--- src/renderer/components/currency/CurrencyInfo.tsx
+++ src/renderer/components/currency/CurrencyInfo.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import { formatBN, bn } from '@xchainjs/xchain-util'
+import { bn, assetAmount, formatAssetAmountCurrency } from '@xchainjs/xchain-util'
 import BigNumber from 'bignumber.js'
 import * as O from 'fp-ts/lib/Option'
 import { pipe } from 'fp-ts/pipeable'
 
+import { ONE_ASSET_AMOUNT } from '../../const'
 import { sequenceTOption } from '../../helpers/fpHelpers'
 import { AssetWithPrice } from '../../services/binance/types'
 import * as Styled from './CurrencyInfo.styles'
@@ -18,17 +19,39 @@ type CurrencyInfoProps = {
 export const CurrencyInfo = ({ to = O.none, from = O.none, slip = bn(0) }: CurrencyInfoProps) => {
   return pipe(
     sequenceTOption(from, to),
-    O.map(([from, to]) => (
-      <Styled.Container key={'currency info'}>
-        <div>
-          1 {from.asset.ticker} = {formatBN(from.priceRune.dividedBy(to.priceRune), 5)} {to.asset.ticker}
-        </div>
-        <div>
-          1 {to.asset.ticker} = {formatBN(to.priceRune.dividedBy(from.priceRune), 5)} {from.asset.ticker}
-        </div>
-        <div>slip: {slip.multipliedBy(100).toFormat(2)}%</div>
-      </Styled.Container>
-    )),
+    O.map(([from, to]) => {
+      return (
+        <Styled.Container key={'currency info'}>
+          <div>
+            {formatAssetAmountCurrency({
+              asset: from.asset,
+              amount: ONE_ASSET_AMOUNT,
+              trimZeros: true
+            })}{' '}
+            ={' '}
+            {formatAssetAmountCurrency({
+              asset: to.asset,
+              amount: assetAmount(from.priceRune.dividedBy(to.priceRune)),
+              trimZeros: true
+            })}
+          </div>
+          <div>
+            {formatAssetAmountCurrency({
+              asset: to.asset,
+              amount: ONE_ASSET_AMOUNT,
+              trimZeros: true
+            })}{' '}
+            ={' '}
+            {formatAssetAmountCurrency({
+              asset: from.asset,
+              amount: assetAmount(to.priceRune.dividedBy(from.priceRune)),
+              trimZeros: true
+            })}
+          </div>
+          <div>slip: {slip.multipliedBy(100).toFormat(2)}%</div>
+        </Styled.Container>
+      )
+    }),
     O.toNullable
   )
 }
