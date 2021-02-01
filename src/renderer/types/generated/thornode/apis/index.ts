44e5bee9d221719f287ae9fa4a5a7f71af246376
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2021-02-01 18:56:34 +0300

    added thornode api generation

---
+++ src/renderer/types/generated/thornode/apis/index.ts
@@ -0,0 +1,7 @@
+export * from './HealthCheckApi';
+export * from './KeygenKeysignApi';
+export * from './NetworkApi';
+export * from './NodesApi';
+export * from './PoolsApi';
+export * from './TxApi';
+export * from './VaultsApi';
