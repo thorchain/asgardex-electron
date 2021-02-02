0bc8f9a442e612f236e004a9179ddc03fc5cfd32
Sarawut Sanit <sanit.sa@outlook.com>
2021-02-02 16:32:25 +0700

    store previous fee in swap (#809)
    * store previous fee in swap
    
    * fix comments
    
    * remove commented style

--- src/renderer/components/uielements/fees/Fees.tsx
+++ src/renderer/components/uielements/fees/Fees.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 
 import * as RD from '@devexperts/remote-data-ts'
 import * as FP from 'fp-ts/function'
 import * as A from 'fp-ts/lib/Array'
+import * as O from 'fp-ts/lib/Option'
 import { useIntl } from 'react-intl'
 
 import { formatFee } from './Fees.helper'
@@ -18,30 +19,53 @@ type Props = {
 export const Fees: React.FC<Props> = ({ fees, reloadFees, className }) => {
   const intl = useIntl()
 
+  const prevFeesRef = useRef<O.Option<string>>(O.none)
+
+  const formattedFees = FP.pipe(
+    fees,
+    RD.map((fees) =>
+      FP.pipe(
+        fees,
+        A.map(formatFee),
+        A.reduceWithIndex(
+          intl.formatMessage({ id: fees.length > 1 ? 'common.fees' : 'common.fee' }),
+          (index, acc, cur) => {
+            return index === 0 ? `${acc}: ${cur}` : `${acc} + ${cur}`
+          }
+        )
+      )
+    )
+  )
+
+  const oFees: O.Option<string> = useMemo(() => FP.pipe(formattedFees, RD.toOption), [formattedFees])
+
+  // Store latest fees as `ref`
+  // needed to display previous fee while reloading
+  useEffect(() => {
+    FP.pipe(
+      oFees,
+      O.map((fees) => (prevFeesRef.current = O.some(fees)))
+    )
+  }, [oFees])
+
   const feesFormattedValue = useMemo(
     () =>
       FP.pipe(
-        fees,
-        RD.map((fees) =>
-          FP.pipe(
-            fees,
-            A.map(formatFee),
-            A.reduceWithIndex(
-              intl.formatMessage({ id: fees.length > 1 ? 'common.fees' : 'common.fee' }),
-              (index, acc, cur) => {
-                return index === 0 ? `${acc}: ${cur}` : `${acc} + ${cur}`
-              }
-            )
-          )
-        ),
+        formattedFees,
         RD.fold(
           () => '...',
-          () => '...',
+          () =>
+            // show previous fees while re-loading
+            FP.pipe(
+              prevFeesRef.current,
+              O.map((fees) => fees),
+              O.getOrElse(() => '...')
+            ),
           (error) => `${intl.formatMessage({ id: 'common.error' })}: ${error.message}`,
           FP.identity
         )
       ),
-    [fees, intl]
+    [formattedFees, intl]
   )
 
   return (
@@ -52,9 +76,12 @@ export const Fees: React.FC<Props> = ({ fees, reloadFees, className }) => {
             e.preventDefault()
             reloadFees()
           }}
+          disabled={RD.isPending(fees)}
         />
       )}
-      {feesFormattedValue}
+      <Styled.FeeLabel isError={RD.isFailure(fees)} isLoading={RD.isPending(fees)}>
+        {feesFormattedValue}
+      </Styled.FeeLabel>
     </Styled.Container>
   )
 }
