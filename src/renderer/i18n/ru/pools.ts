7d4ee2f8472bd096efff8a5f82c0d6696b81c967
thatStrangeGuy <thatStrangeGuy@protonmail.com>
2021-02-04 14:14:07 +0300

    updated RU translations at i18n

--- src/renderer/i18n/ru/pools.ts
+++ src/renderer/i18n/ru/pools.ts
@@ -1,11 +1,11 @@
 import { PoolsMessages } from '../types'
 
 const pools: PoolsMessages = {
-  'pools.depth': 'Depth',
+  'pools.depth': 'Глубина',
   'pools.24hvol': 'Значение за 24ч',
-  'pools.avgsize': 'ср  size',
-  'pools.avgslip': 'ср slip',
-  'pools.blocksleft': 'blocks left',
+  'pools.avgsize': 'Ср. значени',
+  'pools.avgslip': 'Ср. слип',
+  'pools.blocksleft': 'Осталось блоков',
   'pools.trades': 'Сделки',
   'pools.pending': 'Пулы в ожидании',
   'pools.available': 'Доступные пулы'
