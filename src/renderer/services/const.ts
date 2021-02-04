876db301585859e5501515af16f7e0c061a27f4c
junkai121 <junkai121@outlook.com>
2021-02-04 23:07:07 +1000

    fix comments

--- src/renderer/services/const.ts
+++ src/renderer/services/const.ts
@@ -1,9 +1,9 @@
 import { Network } from '../../shared/api/types'
-import { loadEnabledChains } from '../helpers/envHelper'
+import { envOrDefault } from '../helpers/envHelper'
 
 export const DEFAULT_NETWORK: Network = 'testnet'
 export const AVAILABLE_NETWORKS: Network[] = ['testnet', 'chaosnet', 'mainnet']
-export const ENABLED_CHAINS = loadEnabledChains(process.env.REACT_APP_CHAINS_ENABLED)
+export const ENABLED_CHAINS = envOrDefault(process.env.REACT_APP_CHAINS_ENABLED, 'THOR,BNB,BTC').split(',')
 
 // Pagination: max. number of items
 export const MAX_ITEMS_PER_PAGE = 10
