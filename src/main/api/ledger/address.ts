aa9b1d3db384171e2331c1d10c9b2e09b550d24f
veado <61792675+veado@users.noreply.github.com>
2021-02-16 18:08:52 +0100

    Disable `node-hid` related stuff temporary (develop) (#925)
    to fix signing issues we currently have on macOS temporary (see #890)

--- src/main/api/ledger/address.ts
+++ src/main/api/ledger/address.ts
@@ -1,29 +1,32 @@
-import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
-import { BNBChain, BTCChain, Chain } from '@xchainjs/xchain-util'
-import * as E from 'fp-ts/Either'
+// import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
+// import { BNBChain, BTCChain, Chain } from '@xchainjs/xchain-util'
+import { Chain } from '@xchainjs/xchain-util'
+// import * as E from 'fp-ts/Either'
 
-import { LedgerErrorId, Network } from '../../../shared/api/types'
-import { getAddress as getBNBAddress } from './binance'
-import { getAddress as getBTCAddress } from './bitcoin'
-import { getErrorId } from './utils'
+import { Network } from '../../../shared/api/types'
+// import { LedgerErrorId, Network } from '../../../shared/api/types'
+// import { getAddress as getBNBAddress } from './binance'
+// import { getAddress as getBTCAddress } from './bitcoin'
+// import { getErrorId } from './utils'
 
 export const getAddress = async (chain: Chain, network: Network) => {
-  try {
-    const transport = await TransportNodeHid.open('')
-    let res: E.Either<LedgerErrorId, string>
-    switch (chain) {
-      case BNBChain:
-        res = await getBNBAddress(transport, network)
-        break
-      case BTCChain:
-        res = await getBTCAddress(transport, network)
-        break
-      default:
-        res = E.left(LedgerErrorId.NO_APP)
-    }
-    await transport.close()
-    return res
-  } catch (error) {
-    return E.left(getErrorId(error.toString()))
-  }
+  const _disabled = { chain, network }
+  // try {
+  //   const transport = await TransportNodeHid.open('')
+  //   let res: E.Either<LedgerErrorId, string>
+  //   switch (chain) {
+  //     case BNBChain:
+  //       res = await getBNBAddress(transport, network)
+  //       break
+  //     case BTCChain:
+  //       res = await getBTCAddress(transport, network)
+  //       break
+  //     default:
+  //       res = E.left(LedgerErrorId.NO_APP)
+  //   }
+  //   await transport.close()
+  //   return res
+  // } catch (error) {
+  //   return E.left(getErrorId(error.toString()))
+  // }
 }
