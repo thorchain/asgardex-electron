3b7c88600a19975470a3312f962eda83449fc548
veado <61792675+veado@users.noreply.github.com>
2021-02-16 18:07:52 +0100

    Disable `node-hid` related stuff temporary (#923)
    to fix signing issues we currently have on macOS temporary (see #890)

--- src/main/api/ledger/bitcoin.ts
+++ src/main/api/ledger/bitcoin.ts
@@ -1,58 +1,60 @@
-import AppBTC from '@ledgerhq/hw-app-btc'
-import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
-import { broadcastTx, createTxInfo, getDerivePath } from '@xchainjs/xchain-bitcoin'
-import * as E from 'fp-ts/Either'
+// Note: Disabled temporary due sign issues on macOS
 
-import { LedgerBTCTxInfo, LedgerErrorId, Network } from '../../../shared/api/types'
-import { getErrorId } from './utils'
+// import AppBTC from '@ledgerhq/hw-app-btc'
+// import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
+// import { broadcastTx, createTxInfo, getDerivePath } from '@xchainjs/xchain-bitcoin'
+// import * as E from 'fp-ts/Either'
 
-export const getAddress = async (transport: TransportNodeHid, network: Network) => {
-  try {
-    const app = new AppBTC(transport)
-    const derive_path = getDerivePath(0)
-    const info = await app.getWalletPublicKey(network === 'testnet' ? derive_path.testnet : derive_path.mainnet, {
-      format: 'bech32'
-    })
+// import { LedgerBTCTxInfo, LedgerErrorId, Network } from '../../../shared/api/types'
+// import { getErrorId } from './utils'
 
-    return E.right(info.bitcoinAddress)
-  } catch (error) {
-    return E.left(getErrorId(error.toString()))
-  }
-}
+// export const getAddress = async (transport: TransportNodeHid, network: Network) => {
+//   try {
+//     const app = new AppBTC(transport)
+//     const derive_path = getDerivePath(0)
+//     const info = await app.getWalletPublicKey(network === 'testnet' ? derive_path.testnet : derive_path.mainnet, {
+//       format: 'bech32'
+//     })
 
-export const sendTx = async (
-  transport: TransportNodeHid,
-  network: Network,
-  txInfo: LedgerBTCTxInfo
-): Promise<E.Either<LedgerErrorId, string>> => {
-  try {
-    const app = new AppBTC(transport)
-    const derive_path = getDerivePath(0)
-    const { utxos, newTxHex } = await createTxInfo({
-      ...txInfo,
-      network: network === 'testnet' ? 'testnet' : 'mainnet'
-    })
-    const txs = utxos.map((utxo) => {
-      return {
-        tx: app.splitTransaction(utxo.txHex, true),
-        ...utxo
-      }
-    })
-    const newTx = app.splitTransaction(newTxHex, true)
-    const outputScriptHex = app.serializeTransactionOutputs(newTx).toString('hex')
-    const txHex = await app.createPaymentTransactionNew({
-      inputs: txs.map((utxo) => {
-        return [utxo.tx, utxo.index, null, null]
-      }),
-      associatedKeysets: txs.map((_) => (network === 'testnet' ? derive_path.testnet : derive_path.mainnet)),
-      outputScriptHex,
-      segwit: true,
-      additionals: ['bitcoin', 'bech32']
-    })
-    const txHash = await broadcastTx({ txHex, nodeUrl: txInfo.nodeUrl, nodeApiKey: txInfo.nodeApiKey })
+//     return E.right(info.bitcoinAddress)
+//   } catch (error) {
+//     return E.left(getErrorId(error.toString()))
+//   }
+// }
 
-    return E.right(txHash)
-  } catch (error) {
-    return E.left(getErrorId(error.toString()))
-  }
-}
+// export const sendTx = async (
+//   transport: TransportNodeHid,
+//   network: Network,
+//   txInfo: LedgerBTCTxInfo
+// ): Promise<E.Either<LedgerErrorId, string>> => {
+//   try {
+//     const app = new AppBTC(transport)
+//     const derive_path = getDerivePath(0)
+//     const { utxos, newTxHex } = await createTxInfo({
+//       ...txInfo,
+//       network: network === 'testnet' ? 'testnet' : 'mainnet'
+//     })
+//     const txs = utxos.map((utxo) => {
+//       return {
+//         tx: app.splitTransaction(utxo.txHex, true),
+//         ...utxo
+//       }
+//     })
+//     const newTx = app.splitTransaction(newTxHex, true)
+//     const outputScriptHex = app.serializeTransactionOutputs(newTx).toString('hex')
+//     const txHex = await app.createPaymentTransactionNew({
+//       inputs: txs.map((utxo) => {
+//         return [utxo.tx, utxo.index, null, null]
+//       }),
+//       associatedKeysets: txs.map((_) => (network === 'testnet' ? derive_path.testnet : derive_path.mainnet)),
+//       outputScriptHex,
+//       segwit: true,
+//       additionals: ['bitcoin', 'bech32']
+//     })
+//     const txHash = await broadcastTx({ txHex, nodeUrl: txInfo.nodeUrl, nodeApiKey: txInfo.nodeApiKey })
+
+//     return E.right(txHash)
+//   } catch (error) {
+//     return E.left(getErrorId(error.toString()))
+//   }
+// }
