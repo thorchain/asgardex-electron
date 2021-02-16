9bc10e9dac0ad8d6ee7224c55834214191e43b86
Veado <veado@protonmail.com>
2021-02-16 11:56:34 +0100

    Disable `node-hid` related stuff temporary

--- src/main/api/ledger/transaction.ts
+++ src/main/api/ledger/transaction.ts
@@ -1,33 +1,39 @@
-import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
-import { BNBChain, BTCChain, Chain } from '@xchainjs/xchain-util'
+// Note: Disabled temporary due sign issues on macOS
+
+// import TransportNodeHid from '@ledgerhq/hw-transport-node-hid'
+// import { BNBChain, BTCChain, Chain } from '@xchainjs/xchain-util'
+import { Chain } from '@xchainjs/xchain-util'
 import * as E from 'fp-ts/Either'
 
-import { LedgerBNCTxInfo, LedgerBTCTxInfo, LedgerErrorId, LedgerTxInfo, Network } from '../../../shared/api/types'
-import { sendTx as sendBNCTx } from './binance'
-import { sendTx as sendBTCTx } from './bitcoin'
-import { getErrorId } from './utils'
+import { LedgerErrorId, LedgerTxInfo, Network } from '../../../shared/api/types'
+// import { LedgerBNCTxInfo, LedgerBTCTxInfo, LedgerErrorId, LedgerTxInfo, Network } from '../../../shared/api/types'
+// import { sendTx as sendBNCTx } from './binance'
+// import { sendTx as sendBTCTx } from './bitcoin'
+// import { getErrorId } from './utils'
 
 export const sendTx = async (
   chain: Chain,
   network: Network,
   txInfo: LedgerTxInfo
 ): Promise<E.Either<LedgerErrorId, string>> => {
-  try {
-    const transport = await TransportNodeHid.open('')
-    let res: E.Either<LedgerErrorId, string>
-    switch (chain) {
-      case BNBChain:
-        res = await sendBNCTx(transport, network, txInfo as LedgerBNCTxInfo)
-        break
-      case BTCChain:
-        res = await sendBTCTx(transport, network, txInfo as LedgerBTCTxInfo)
-        break
-      default:
-        res = E.left(LedgerErrorId.NO_APP)
-    }
-    await transport.close()
-    return res
-  } catch (error) {
-    return E.left(getErrorId(error.toString()))
-  }
+  const _disabled = { chain, network, txInfo }
+  return Promise.reject(Error('sendTx for Ledger is disabled temporary'))
+  // try {
+  //   const transport = await TransportNodeHid.open('')
+  //   let res: E.Either<LedgerErrorId, string>
+  //   switch (chain) {
+  //     case BNBChain:
+  //       res = await sendBNCTx(transport, network, txInfo as LedgerBNCTxInfo)
+  //       break
+  //     case BTCChain:
+  //       res = await sendBTCTx(transport, network, txInfo as LedgerBTCTxInfo)
+  //       break
+  //     default:
+  //       res = E.left(LedgerErrorId.NO_APP)
+  //   }
+  //   await transport.close()
+  //   return res
+  // } catch (error) {
+  //   return E.left(getErrorId(error.toString()))
+  // }
 }
