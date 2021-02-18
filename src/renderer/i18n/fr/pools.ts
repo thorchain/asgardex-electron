2fd425b562f2ea49a60a5b2f9c734c608e0c900f
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-18 19:41:43 +0100

    Create pools.ts

---
+++ src/renderer/i18n/fr/pools.ts
@@ -0,0 +1,14 @@
+import { PoolsMessages } from '../types'
+
+const pools: PoolsMessages = {
+  'pools.depth': 'Depth',
+  'pools.24hvol': '24h volume',
+  'pools.avgsize': 'avg size',
+  'pools.avgfee': 'avg fee',
+  'pools.blocksleft': 'blocks left',
+  'pools.trades': 'Trades',
+  'pools.pending': 'Pending',
+  'pools.available': 'Available'
+}
+
+export default pools
