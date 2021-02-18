0866fb965e8187552110c3b7e6d889095dc034eb
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-18 19:43:21 +0100

    Create settings.ts

---
+++ src/renderer/i18n/fr/settings.ts
@@ -0,0 +1,17 @@
+import { SettingMessages } from '../types'
+
+const settings: SettingMessages = {
+  'setting.title': 'Settings',
+  'setting.wallet.management': 'Wallet Management',
+  'setting.client': 'Client',
+  'setting.account.management': 'Account Management',
+  'setting.export': 'Export Keystore',
+  'setting.lock': 'Lock',
+  'setting.view.phrase': 'View Phrase',
+  'setting.midgard': 'Midgard',
+  'setting.version': 'Version',
+  'setting.notconnected': 'Not connected',
+  'setting.add.device': 'ADD DEVICE'
+}
+
+export default settings
