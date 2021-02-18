7c172012d7558c422c293580f99f7c574cc34a50
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-18 19:41:13 +0100

    Create midgard.ts

---
+++ src/renderer/i18n/fr/midgard.ts
@@ -0,0 +1,8 @@
+import { MidgardMessages } from '../types'
+
+const midgard: MidgardMessages = {
+  'midgard.error.byzantine.title': 'Midgard Byzantine Error',
+  'midgard.error.byzantine.description': 'Could not reach consensus while trying to find endpoint for Midgard API'
+}
+
+export default midgard
