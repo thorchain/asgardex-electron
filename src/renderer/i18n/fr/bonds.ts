f176effb0aa5573ad845a171c73d2068c962f96a
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-19 16:10:14 +0100

    Update bonds.ts

--- src/renderer/i18n/fr/bonds.ts
+++ src/renderer/i18n/fr/bonds.ts
@@ -1,15 +1,15 @@
 import { BondsMessages } from '../types'
 
 const bonds: BondsMessages = {
-  'bonds.node': 'Node',
-  'bonds.bond': 'Bond',
-  'bonds.award': 'Award',
+  'bonds.node': 'Nœud',
+  'bonds.bond': 'Caution',
+  'bonds.award': 'Récompense',
   'bonds.info': 'Info',
-  'bonds.status': 'Status',
-  'bonds.status.active': 'Active',
-  'bonds.status.standby': 'Standby',
-  'bonds.status.disabled': 'Disabled',
-  'bonds.status.unknown': 'Unknown',
+  'bonds.status': 'Statut',
+  'bonds.status.active': 'Actif',
+  'bonds.status.standby': 'En attente',
+  'bonds.status.disabled': 'Hors service',
+  'bonds.status.unknown': 'Inconnu',
   'bonds.node.add': 'Add node',
   'bonds.node.enterMessage': 'Enter node to monitor',
   'bonds.validations.nodeAlreadyAdded': 'Node is already added',
