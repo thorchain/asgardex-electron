ba3fae1e44f6835b01bff797850eeff8b4790f11
junkai121 <junkai121@outlook.com>
2021-02-19 10:29:46 +1000

    update assetIcon story

--- src/renderer/components/uielements/assets/assetIcon/AssetIcon.stories.tsx
+++ src/renderer/components/uielements/assets/assetIcon/AssetIcon.stories.tsx
@@ -28,22 +28,26 @@ storiesOf('Components/assets/AssetIcon', module).add('default', () => {
         <AssetIcon asset={AssetBTC} size={size} network="mainnet" />
         <AssetIcon asset={AssetBCH} size={size} network="mainnet" />
         <AssetIcon asset={AssetBNB} size={size} network="mainnet" />
-
         <AssetIcon asset={ASSETS_MAINNET.BOLT} size={size} network="mainnet" />
         <AssetIcon asset={ASSETS_MAINNET.FTM} size={size} network="mainnet" />
         <AssetIcon asset={ASSETS_MAINNET.TOMO} size={size} network="mainnet" />
         <AssetIcon asset={AssetETH} size={size} network="mainnet" />
         <AssetIcon asset={ERC20_MAINNET.USDT} size={size} network="mainnet" />
         <AssetIcon asset={ERC20_MAINNET.RUNE} size={size} network="mainnet" />
-
         <AssetIcon asset={AssetRuneNative} size={size} network="mainnet" />
+      </div>
 
+      <div style={{ display: 'flex' }}>
+        <AssetIcon asset={AssetBTC} size={size} network="testnet" />
+        <AssetIcon asset={AssetBCH} size={size} network="testnet" />
+        <AssetIcon asset={AssetBNB} size={size} network="testnet" />
         <AssetIcon asset={ASSETS_TESTNET.BOLT} size={size} network="testnet" />
         <AssetIcon asset={ASSETS_TESTNET.FTM} size={size} network="testnet" />
         <AssetIcon asset={ASSETS_TESTNET.TOMO} size={size} network="testnet" />
-
+        <AssetIcon asset={AssetETH} size={size} network="testnet" />
         <AssetIcon asset={ERC20_TESTNET.USDT} size={size} network="testnet" />
         <AssetIcon asset={ERC20_TESTNET.RUNE} size={size} network="testnet" />
+        <AssetIcon asset={AssetRuneNative} size={size} network="testnet" />
       </div>
     </div>
   )
