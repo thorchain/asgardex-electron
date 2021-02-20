31ec59b03b5630958ff1c4d1cfbcc3339a02fc95
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-20 11:22:32 +0100

    Update routes.ts

--- src/renderer/i18n/fr/routes.ts
+++ src/renderer/i18n/fr/routes.ts
@@ -1,7 +1,7 @@
 import { RoutesMessages } from '../types'
 
 const messages: RoutesMessages = {
-  'routes.invalid.asset': 'Actif non valide durant la liaison : {asset}'
+  'routes.invalid.asset': 'Actif non valide durant le routage : {asset}'
 }
 
 export default messages
