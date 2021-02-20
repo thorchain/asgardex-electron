91ba210a63128b3ca9074344c507588f3842e0f2
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-20 14:59:40 +0100

    Update wallet.ts

--- src/renderer/i18n/fr/wallet.ts
+++ src/renderer/i18n/fr/wallet.ts
@@ -49,7 +49,7 @@ const wallet: WalletMessages = {
   'wallet.send.average': 'Normal',
   'wallet.errors.balancesFailed': 'Échec lors du chargement des soldes. {errorMsg} (API Id: {apiId})',
   'wallet.errors.asset.notExist': 'Aucun {asset}',
-  'wallet.errors.address.empty': "L'adresse ne peut pas être vide",
+  'wallet.errors.address.empty': "L'adresse ne peut être vide",
   'wallet.errors.address.invalid': "L'adresse est incorrecte",
   'wallet.errors.address.couldNotFind': "Impossible de trouver l'adresse de la pool {pool}",
   'wallet.errors.amount.shouldBeNumber': 'Le montant doit être un nombre',
