0b7a119984a753b672f0df78759c9ac7aef36d9f
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-20 15:07:02 +0100

    Update pools.ts

--- src/renderer/i18n/fr/pools.ts
+++ src/renderer/i18n/fr/pools.ts
@@ -4,7 +4,7 @@ const pools: PoolsMessages = {
   'pools.depth': 'Profondeur',
   'pools.24hvol': 'volume sur 24h',
   'pools.avgsize': 'taille moy.',
-  'pools.avgfee': 'frais moy.',
+  'pools.avgfee': 'rendement moy.',
   'pools.blocksleft': 'blocs restants',
   'pools.trades': 'Trades',
   'pools.pending': 'En attente',
