ad23f9502886df81592e5fd9ebba4f81cc546d10
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-25 17:31:26 +0100

    French translation (#957)

---
+++ src/renderer/i18n/fr/bonds.ts
@@ -0,0 +1,19 @@
+import { BondsMessages } from '../types'
+
+const bonds: BondsMessages = {
+  'bonds.node': 'Node',
+  'bonds.bond': 'Caution',
+  'bonds.award': 'Récompense',
+  'bonds.info': 'Info',
+  'bonds.status': 'Statut',
+  'bonds.status.active': 'Actif',
+  'bonds.status.standby': 'En attente',
+  'bonds.status.disabled': 'Désactivé',
+  'bonds.status.unknown': 'Inconnu',
+  'bonds.node.add': 'Ajouter un node',
+  'bonds.node.enterMessage': 'Entrez le node à surveiller',
+  'bonds.validations.nodeAlreadyAdded': 'Le node est déjà ajouté',
+  'bonds.node.removeMessage': 'Êtes-vous sûr de vouloir supprimer le node {node} ?'
+}
+
+export default bonds
