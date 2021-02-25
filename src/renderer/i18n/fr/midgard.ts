6f4c75edf2b30acb5651b9e2f9604b05492b9d40
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-02-25 14:00:01 +0100

    Update midgard.ts

--- src/renderer/i18n/fr/midgard.ts
+++ src/renderer/i18n/fr/midgard.ts
@@ -2,7 +2,7 @@ import { MidgardMessages } from '../types'
 
 const midgard: MidgardMessages = {
   'midgard.error.byzantine.title': 'Erreur Byzantine Midgard',
-  'midgard.error.byzantine.description': "Impossible d'atteindre le consensus en tentant de trouver un points d'accès à l'API Midgar"
+  'midgard.error.byzantine.description': "Impossible d'atteindre le consensus en tentant de trouver un points d'accès à l'API Midgard"
 }
 
 export default midgard
