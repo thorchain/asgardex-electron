4c40c518da494406c86e59c260eede54eedbe210
Veado <veado@protonmail.com>
2021-03-01 19:20:35 +0100

    Remove commented import

--- src/renderer/services/bitcoincash/index.ts
+++ src/renderer/services/bitcoincash/index.ts
@@ -1,7 +1,6 @@
 import { balances$, reloadBalances } from './balances'
 import { client$, clientViewState$, address$, explorerUrl$, getExplorerTxUrl$, getExplorerAddressUrl$ } from './common'
 import { createFeesService } from './fees'
-// import { createTransactionService } from './transaction'
 
 const { fees$, feesWithRates$, reloadFees } = createFeesService(client$)
 
