6871427d7749ab6cec235744cc3c8404544e896a
Veado <veado@protonmail.com>
2021-03-01 13:10:32 +0100

    Enable LTC

--- src/renderer/services/const.ts
+++ src/renderer/services/const.ts
@@ -5,7 +5,7 @@ import { envOrDefault } from '../helpers/envHelper'
 
 export const DEFAULT_NETWORK: Network = 'testnet'
 export const AVAILABLE_NETWORKS: Network[] = ['testnet', 'chaosnet', 'mainnet']
-export const ENABLED_CHAINS: Chain[] = envOrDefault(process.env.REACT_APP_CHAINS_ENABLED, 'THOR,BNB,BTC')
+export const ENABLED_CHAINS: Chain[] = envOrDefault(process.env.REACT_APP_CHAINS_ENABLED, 'THOR,BNB,BTC,LTC')
   .replace(/\s/g, '')
   .split(',')
   .filter(isChain)
