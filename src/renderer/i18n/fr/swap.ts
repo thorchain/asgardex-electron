44c81259cd6f6f25ec907557a2532a7042787ef7
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-03-04 20:38:33 +0100

    Update swap.ts

--- src/renderer/i18n/fr/swap.ts
+++ src/renderer/i18n/fr/swap.ts
@@ -1,20 +1,20 @@
 import { SwapMessages } from '../types'
 
 const swap: SwapMessages = {
-  'swap.state.pending': 'Swap en cours',
-  'swap.state.success': 'Swap réussi',
-  'swap.state.error': 'Erreur lors du swap',
+  'swap.state.pending': 'Échange en cours',
+  'swap.state.success': 'Échange réussi',
+  'swap.state.error': "Erreur lors de l'échange",
   'swap.input': 'Entrée',
   'swap.balance': 'Solde',
   'swap.output': 'Sortie',
-  'swap.drag': 'Glisser pour swapper',
+  'swap.drag': 'Glisser pour échanger',
   'swap.searchAsset': 'Rechercher un actif',
   'swap.errors.amount.balanceShouldCoverChainFee':
     '{fee} de frais de transaction doivent être couverts par votre solde (actuellement {balance}).',
   'swap.errors.amount.outputShouldCoverChainFee':
     '{fee} de frais de sortie doivent être couverts par la réception du montant (actuellement {amount}).',
-  'swap.note.lockedWallet': 'Vous devez déverrouiller votre wallet pour swapper',
-  'swap.note.nowallet': 'Créer ou importer un wallet pour swapper',
+  'swap.note.lockedWallet': 'Vous devez déverrouiller votre portefeuille pour échanger',
+  'swap.note.nowallet': 'Créez ou importez un portefeuille pour échanger',
   'swap.errors.asset.missingSourceAsset': 'Actif source manquant',
   'swap.errors.asset.missingTargetAsset': 'Actif cible manquant'
 }
