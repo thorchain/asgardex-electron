2163b7c711b489686b4dbe6aa57d202ba0b18f3c
junkai121 <39806640+junkai121@users.noreply.github.com>
2021-03-05 02:02:29 +1000

    improve token row, fix output amount font (#1034)

--- src/renderer/components/swap/Swap.tsx
+++ src/renderer/components/swap/Swap.tsx
@@ -9,10 +9,11 @@ import {
   formatBN,
   baseToAsset,
   BaseAmount,
-  formatAssetAmountCurrency,
   AssetRuneNative,
   baseAmount,
-  assetAmount
+  assetAmount,
+  formatAssetAmount,
+  formatAssetAmountCurrency
 } from '@xchainjs/xchain-util'
 import { eqString } from 'fp-ts/Eq'
 import * as A from 'fp-ts/lib/Array'
@@ -617,13 +618,10 @@ export const Swap = ({
   const swapResultLabel = useMemo(
     () =>
       FP.pipe(
-        targetAsset,
-        O.map((asset) =>
-          formatAssetAmountCurrency({ amount: baseToAsset(swapData.swapResult), asset, trimZeros: true })
-        ),
+        O.some(formatAssetAmount({ amount: baseToAsset(swapData.swapResult), trimZeros: true })),
         O.getOrElse(() => formatBN(ZERO_BN))
       ),
-    [targetAsset, swapData]
+    [swapData]
   )
 
   const targetChainFeeErrorLabel = useMemo(() => {
