29652beafc0cb349e3ae3ac8c40921f006429144
sarawut <sanit.sa@outlook.com>
2021-03-06 01:30:31 +0700

    fix comments

--- src/renderer/helpers/walletHelper.ts
+++ src/renderer/helpers/walletHelper.ts
@@ -32,9 +32,7 @@ export const getWalletBalanceByAsset = (
     O.chain(([walletBalances, asset]) =>
       FP.pipe(
         walletBalances,
-        A.findFirst(({ asset: assetInList }) =>
-          eqAsset.equals({ ...assetInList, symbol: assetInList.symbol.toUpperCase() }, asset)
-        )
+        A.findFirst(({ asset: assetInList }) => eqAsset.equals(assetInList, asset))
       )
     )
   )
