43926f85d5a7fd27fe895469c9e30d02af7f517e
junkai121 <junkai121@outlook.com>
2021-03-16 03:49:39 +1000

    add comment

--- src/renderer/services/thorchain/balances.ts
+++ src/renderer/services/thorchain/balances.ts
@@ -8,6 +8,7 @@ import { client$ } from './common'
 const { stream$: reloadBalances$, trigger: reloadBalances } = triggerStream()
 
 // State of balances loaded by Client
+// Currently in ASGDX `AssetRuneNative` is supported only. Remove asset list if we want to get balances of all assets at THORChain.
 const balances$: C.WalletBalancesLD = C.balances$(client$, reloadBalances$, [AssetRuneNative])
 
 export { balances$, reloadBalances }
