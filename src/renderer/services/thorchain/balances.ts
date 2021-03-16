e1ce9f6a1fcf1c08823a22b026dfe8f3550ecd7a
junkai121 <39806640+junkai121@users.noreply.github.com>
2021-03-16 10:35:42 +1000

    Filter only RUNE for Thorchain (#1109)
    * filter only rune for thorchain
    
    * add comment

--- src/renderer/services/thorchain/balances.ts
+++ src/renderer/services/thorchain/balances.ts
@@ -1,3 +1,5 @@
+import { AssetRuneNative } from '@xchainjs/xchain-util'
+
 import { triggerStream } from '../../helpers/stateHelper'
 import * as C from '../clients'
 import { client$ } from './common'
@@ -6,6 +8,7 @@ import { client$ } from './common'
 const { stream$: reloadBalances$, trigger: reloadBalances } = triggerStream()
 
 // State of balances loaded by Client
-const balances$: C.WalletBalancesLD = C.balances$(client$, reloadBalances$)
+// Currently in ASGDX `AssetRuneNative` is supported only. Remove asset list if we want to get balances of all assets at THORChain.
+const balances$: C.WalletBalancesLD = C.balances$(client$, reloadBalances$, [AssetRuneNative])
 
 export { balances$, reloadBalances }
