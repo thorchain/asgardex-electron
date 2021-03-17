00504ab4c9d6169d2011398f6c92772f72f0ed43
sarawut <sanit.sa@outlook.com>
2021-03-17 09:17:56 +0700

    fix ETH txStatus

--- src/renderer/services/chain/transaction/withdraw.ts
+++ src/renderer/services/chain/transaction/withdraw.ts
@@ -5,7 +5,7 @@ import * as O from 'fp-ts/lib/Option'
 import * as Rx from 'rxjs'
 import * as RxOp from 'rxjs/operators'
 
-import { getEthAssetAddress, isRuneNativeAsset } from '../../../helpers/assetHelper'
+import { getEthAssetAddress, isEthAsset, isRuneNativeAsset } from '../../../helpers/assetHelper'
 import { isEthChain } from '../../../helpers/chainHelper'
 import { liveData } from '../../../helpers/rx/liveData'
 import { observableState } from '../../../helpers/stateHelper'
@@ -74,7 +74,8 @@ export const withdraw$ = ({ poolAddress, asset, memo, network }: WithdrawParams)
         withdrawTx: RD.success(txHash)
       })
       // 3. check tx finality by polling its tx data
-      const assetAddress: O.Option<Address> = isEthChain(asset.chain) ? getEthAssetAddress(asset) : O.none
+      const assetAddress: O.Option<Address> =
+        isEthChain(asset.chain) && !isEthAsset(asset) ? getEthAssetAddress(asset) : O.none
       return poolTxStatusByChain$({ txHash, chain: asset.chain, assetAddress })
     }),
     liveData.map((_) => setState({ ...getState(), withdraw: RD.success(true) })),
