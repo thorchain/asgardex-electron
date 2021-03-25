a1f4b5a5b11fd8223ea5a80b6665946e6524626a
sarawut <sanit.sa@outlook.com>
2021-03-25 21:28:20 +0700

    add test

--- src/renderer/routes/pools/pools.test.ts
+++ src/renderer/routes/pools/pools.test.ts
@@ -1,4 +1,4 @@
-import { base } from './pools'
+import { base, deposit, swap, poolDetail } from './pools'
 
 describe('Pools routes', () => {
   describe('base route', () => {
@@ -7,6 +7,46 @@ describe('Pools routes', () => {
     })
     it('path', () => {
       expect(base.path()).toEqual('/pools')
+    })
+  })
+
+  describe('Deposit routes', () => {
+    it('template', () => {
+      expect(deposit.template).toEqual('/pools/deposit/:asset')
+    })
+    it('returns path by given asset parameter', () => {
+      expect(deposit.path({ asset: 'BNB' })).toEqual('/pools/deposit/bnb')
+    })
+    it('redirects to base path if asset is empty', () => {
+      expect(deposit.path({ asset: '' })).toEqual('/pools/deposit')
+    })
+  })
+
+  describe('Swap routes', () => {
+    it('template', () => {
+      expect(swap.template).toEqual('/pools/swap/:source|:target')
+    })
+    it('returns path by given source/target parameters', () => {
+      expect(swap.path({ source: 'BNB', target: 'RUNE' })).toEqual('/pools/swap/bnb|rune')
+    })
+    it('redirects to base path if source is empty', () => {
+      expect(swap.path({ source: '', target: 'RUNE' })).toEqual('/pools/swap')
+    })
+    it('redirects to base path if target is empty', () => {
+      expect(swap.path({ source: 'bnb', target: '' })).toEqual('/pools/swap')
+    })
+  })
+
+  describe('PoolDetail routes', () => {
+    it('template', () => {
+      expect(poolDetail.template).toEqual('/pools/detail/:symbol')
+    })
+    it('returns path by given symbol parameter', () => {
+      expect(poolDetail.path({ symbol: 'BNB' })).toEqual('/pools/detail/BNB')
+    })
+    it('redirects to base path if symbol is empty', () => {
+      console.log(123, poolDetail.path({ symbol: '' }))
+      expect(poolDetail.path({ symbol: '' })).toEqual('/pools/detail')
     })
   })
 })
