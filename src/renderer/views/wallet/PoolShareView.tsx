b14b7f5f38343fe4847c1b903765ac4a1cda74e2
Veado <veado@protonmail.com>
2021-03-26 19:00:39 +0100

    Reload latest pool data every time by entering
    the view

--- src/renderer/views/wallet/PoolShareView.tsx
+++ src/renderer/views/wallet/PoolShareView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef } from 'react'
+import React, { useCallback, useMemo, useRef, useEffect } from 'react'
 
 import { SyncOutlined } from '@ant-design/icons'
 import * as RD from '@devexperts/remote-data-ts'
@@ -42,6 +42,11 @@ export const PoolShareView: React.FC = (): JSX.Element => {
   const thorchainContext = useThorchainContext()
   const oRuneAddress = useObservableState(thorchainContext.address$, O.none)
   const { addressByChain$ } = useChainContext()
+
+  useEffect(() => {
+    reloadAllPools()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const [poolSharesRD]: [PoolSharesRD, unknown] = useObservableState(
     () =>
