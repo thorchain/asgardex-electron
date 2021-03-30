79c9dcb94e91618a5b42324e64654794824e8399
veado <61792675+veado@users.noreply.github.com>
2021-03-30 19:34:28 +0200

    Add missing menu for FR (#1227)

---
+++ src/main/i18n/fr/index.ts
@@ -0,0 +1,36 @@
+import { EditMenuMessages, HelpMenuMessages, ViewMenuMessages, AppMenuMessages } from '../types'
+
+const appMenu: AppMenuMessages = {
+  'menu.app.about': 'À propos de {name}',
+  'menu.app.hideApp': 'Masquer {name}',
+  'menu.app.hideOthers': 'Masquer les autres',
+  'menu.app.unhide': 'Montrer tout',
+  'menu.app.quit': 'Quitter {name}'
+}
+
+const editMenu: EditMenuMessages = {
+  'menu.edit.title': 'Edition',
+  'menu.edit.undo': 'Annuler',
+  'menu.edit.redo': 'Rétablir',
+  'menu.edit.cut': 'Couper',
+  'menu.edit.copy': 'Copier',
+  'menu.edit.paste': 'Coller',
+  'menu.edit.selectAll': 'Sélectionner tout'
+}
+
+const helpMenu: HelpMenuMessages = {
+  'menu.help.title': 'Aide',
+  'menu.help.learn': 'En apprendre davantage...',
+  'menu.help.docs': 'Documentation',
+  'menu.help.telegram': 'Telegram',
+  'menu.help.issues': 'Signaler un problème'
+}
+
+const viewMenu: ViewMenuMessages = {
+  'menu.view.title': 'Affichage',
+  'menu.view.reload': 'Recharger',
+  'menu.view.toggleFullscreen': 'Basculer en plein écran',
+  'menu.view.toggleDevTools': 'Basculer en outils de développement'
+}
+
+export default { ...appMenu, ...editMenu, ...viewMenu, ...helpMenu }
--- src/main/i18n/index.ts
+++ src/main/i18n/index.ts
@@ -3,17 +3,20 @@ import { createIntlCache } from 'react-intl'
 import { Locale } from '../../shared/i18n/types'
 import de from './de'
 import en from './en'
+import fr from './fr'
 import ru from './ru'
 import { Messages } from './types'
 
 export const getMessagesByLocale = (l: Locale): Messages => {
   switch (l) {
-    case Locale.EN:
-      return en
-    case Locale.RU:
-      return ru
     case Locale.DE:
       return de
+    case Locale.EN:
+      return en
+    case Locale.FR:
+      return fr
+    case Locale.RU:
+      return ru
     default:
       return en
   }
