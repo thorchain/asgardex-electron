0760718c7d094f49f1dc7525d9438fbd487c3ead
Sarawut <sanit.sa@outlook.com>
2021-04-08 16:27:12 +0700

    add refresh button in swap page (#1283)

--- src/renderer/views/swap/SwapView.styles.ts
+++ src/renderer/views/swap/SwapView.styles.ts
@@ -1,5 +1,19 @@
+import * as A from 'antd'
 import styled from 'styled-components'
 import { palette } from 'styled-theme'
+
+import { BackLink as BackLinkUI } from '../../components/uielements/backLink'
+
+export const TopControlsContainer = styled(A.Row).attrs({
+  justify: 'space-between',
+  align: 'middle'
+})`
+  margin-bottom: 20px;
+`
+
+export const BackLink = styled(BackLinkUI)`
+  margin: 0 !important;
+`
 
 export const ContentContainer = styled('div')`
   flex: 1;
