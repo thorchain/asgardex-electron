69a74d7a0821876439d091de4c2d0bd8baa42031
Veado <veado@protonmail.com>
2021-04-13 14:15:57 +0200

    Prepare 0.1.0

--- src/renderer/services/const.ts
+++ src/renderer/services/const.ts
@@ -3,7 +3,7 @@ import { isChain, Chain } from '@xchainjs/xchain-util'
 import { Network } from '../../shared/api/types'
 import { envOrDefault } from '../helpers/envHelper'
 
-export const DEFAULT_NETWORK: Network = 'testnet'
+export const DEFAULT_NETWORK: Network = 'chaosnet'
 export const AVAILABLE_NETWORKS: Network[] = ['testnet', 'chaosnet'] // ['testnet', 'chaosnet', 'mainnet']
 export const ENABLED_CHAINS: Chain[] = envOrDefault(process.env.REACT_APP_CHAINS_ENABLED, 'THOR,BNB,BTC,LTC,BCH,ETH')
   .replace(/\s/g, '')
