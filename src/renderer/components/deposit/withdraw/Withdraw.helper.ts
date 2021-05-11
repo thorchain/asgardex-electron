991d722052a45da62b7d588b1b22bb81bd3cbc7d
Veado <veado@protonmail.com>
2021-05-11 10:53:05 +0200

    Add `minAssetAmountToWithdrawMax1e8` + `minRuneAmountToWithdraw`
    to Withdraw.helper

--- src/renderer/components/deposit/withdraw/Withdraw.helper.ts
+++ src/renderer/components/deposit/withdraw/Withdraw.helper.ts
@@ -1,9 +1,12 @@
-import { BaseAmount, baseAmount, bn } from '@xchainjs/xchain-util'
+import { Asset, BaseAmount, baseAmount, bn } from '@xchainjs/xchain-util'
 import * as FP from 'fp-ts/lib/function'
 import * as O from 'fp-ts/lib/Option'
 
 import { ZERO_BASE_AMOUNT, ZERO_BN } from '../../../const'
+import { isChainAsset, max1e8BaseAmount } from '../../../helpers/assetHelper'
+import { priceFeeAmountForAsset } from '../../../services/chain/fees/utils'
 import { WithdrawFees } from '../../../services/chain/types'
+import { PoolsDataMap } from '../../../services/midgard/types'
 
 export const getWithdrawAmounts = (
   runeShare: BaseAmount,
@@ -43,3 +46,48 @@ export const getAsymWithdrawAmount = ({
 
 export const sumWithdrawFees = ({ inFee, outFee }: Pick<WithdrawFees, 'inFee' | 'outFee'>): BaseAmount =>
   inFee.plus(outFee)
+
+/**
+ * Returns min. amount for asset to withdraw to cover outbound fees
+ */
+export const minAssetAmountToWithdrawMax1e8 = ({
+  fees,
+  asset,
+  assetDecimal,
+  poolsData
+}: {
+  fees: Pick<WithdrawFees, 'outFee' | 'asset'>
+  /* asset to withdraw */
+  asset: Asset
+  assetDecimal: number
+  poolsData: PoolsDataMap
+}): BaseAmount => {
+  const { asset: feeAsset, outFee } = fees
+
+  const outFeeInAsset = isChainAsset(asset)
+    ? outFee
+    : priceFeeAmountForAsset({
+        feeAmount: outFee,
+        feeAsset,
+        asset,
+        assetDecimal,
+        poolsData
+      })
+
+  return FP.pipe(
+    // Over-estimate fee by 50%
+    1.5,
+    outFeeInAsset.times,
+    // transform decimal to be `max1e8`
+    max1e8BaseAmount
+  )
+}
+
+/**
+ * Returns min. amount for RUNE to withdraw to cover outbound fees
+ */
+export const minRuneAmountToWithdraw = (fees: Pick<WithdrawFees, 'outFee'>): BaseAmount => {
+  const { outFee } = fees
+  // Over-estimate balance by 50%
+  return outFee.times(1.5)
+}
