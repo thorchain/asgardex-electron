474f13dc6f962a3bb3067053de2a51aa650044dd
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-05-25 20:54:37 +0200

    Update update.ts

--- src/renderer/i18n/fr/update.ts
+++ src/renderer/i18n/fr/update.ts
@@ -4,8 +4,8 @@ const update: UpdateMessages = {
   'update.description': 'Nouvelle version {version} disponible',
   'update.link': 'Page de mise à jour',
   'update.checkFailed': 'Erreur pendant la vérification de mise à jour : {error}',
-  'update.checkForUpdates': 'Vérification des mises à jour',
-  'update.noUpdate': 'No update available - FR'
+  'update.checkForUpdates': 'Vérifier les mises à jour',
+  'update.noUpdate': 'Aucune mise à jour disponible'
 }
 
 export default update
