08a945a35761f3a0c3563e61c56239eb745c8a55
Warren Shen <warrenzshen@gmail.com>
2021-05-31 23:31:11 -0700

    Fix alignment / spacing issue in "Pool Details" view (#1473)
    * Fix alignment / spacing issue in pool details view
    
    * Remove outdated css

--- src/renderer/components/pool/PoolCards.style.tsx
+++ src/renderer/components/pool/PoolCards.style.tsx
@@ -1,18 +1,38 @@
 import * as A from 'antd'
 import styled from 'styled-components'
 
+import { media } from '../../helpers/styleHelper'
+
 const ITEM_GAP = '8px'
 
 export const Container = styled(A.Row)`
-  // Needed to compensate extra margin caused by last items of Col
-  margin-bottom: -${ITEM_GAP};
+  height: 100%;
 `
 
 export const Col = styled(A.Col).attrs({
   span: 12
 })`
-  // Need to use paddings instead of margins 'cause
-  // of Ant.Col width-value calculation strategy
-  padding-right: ${ITEM_GAP};
   margin-bottom: ${ITEM_GAP};
+
+  // For larger views, this removes the margin from the last row of items.
+  :nth-last-child(-n + 2) {
+    ${media.md`
+      margin-bottom: 0px;
+    `}
+  }
+
+  // Note: we use padding-right instead of margin-right to
+  // work with the Ant.Col width-value calculation strategy.
+  padding-right: ${ITEM_GAP};
+
+  :nth-child(even) {
+    // For smaller views: we do not have any padding-right since
+    // are no elements to the right of the cards.
+    padding-right: 0px;
+    ${media.md`
+      // For larger views: we have padding-right since there are
+      // elements to the right of the cards.
+      padding-right: ${ITEM_GAP};
+    `}
+  }
 `
