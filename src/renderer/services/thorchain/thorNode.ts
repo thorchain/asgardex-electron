9a9cdecca0bf15c3437ef150962252620eb4a977
thatStrangeGuyThorchain <67793194+thatStrangeGuyThorchain@users.noreply.github.com>
2021-06-03 20:44:04 +0300

    Resolve: [Deposit] Add business logic to check pending assets  (#1503)

--- src/renderer/services/thorchain/thorNode.ts
+++ src/renderer/services/thorchain/thorNode.ts
@@ -1,9 +1,11 @@
 import * as RD from '@devexperts/remote-data-ts'
 import { Address } from '@xchainjs/xchain-client'
-import { baseAmount } from '@xchainjs/xchain-util'
+import { assetToString, baseAmount } from '@xchainjs/xchain-util'
+import * as A from 'fp-ts/Array'
+import * as E from 'fp-ts/Either'
 import * as FP from 'fp-ts/function'
-import * as E from 'fp-ts/lib/Either'
-import { Errors } from 'io-ts'
+import * as O from 'fp-ts/Option'
+import * as t from 'io-ts'
 import { PathReporter } from 'io-ts/lib/PathReporter'
 import * as Rx from 'rxjs'
 import * as RxAjax from 'rxjs/ajax'
@@ -12,11 +14,24 @@ import * as RxOp from 'rxjs/operators'
 import { Network } from '../../../shared/api/types'
 import { THORCHAIN_DECIMAL } from '../../helpers/assetHelper'
 import { envOrDefault } from '../../helpers/envHelper'
+import { eqOString } from '../../helpers/fp/eq'
 import { liveData } from '../../helpers/rx/liveData'
 import { triggerStream } from '../../helpers/stateHelper'
 import { network$ } from '../app/service'
 import { ErrorId } from '../wallet/types'
-import { MimirIO, Mimir, MimirLD, NodeInfoLD, NodeStatus, ThorNodeApiUrlLD } from './types'
+import {
+  MimirIO,
+  Mimir,
+  MimirLD,
+  NodeInfoLD,
+  NodeStatus,
+  ThorNodeApiUrlLD,
+  LiquidityProviderIO,
+  LiquidityProvidersLD,
+  LiquidityProviderLD,
+  GetLiquidityProvidersParams,
+  GetLiquidityProviderParams
+} from './types'
 
 // Tmp type as ThorNodeApi does not provide valid swagger spec yet
 // TODO remove after https://github.com/thorchain/asgardex-electron/issues/763
@@ -103,6 +118,74 @@ const getNodeInfo$ = (node: Address, network: Network): NodeInfoLD =>
     RxOp.startWith(RD.pending)
   )
 
+const { stream$: reloadLiquidityProviders$, trigger: reloadLiquidityProviders } = triggerStream()
+
+const getLiquidityProviders = ({
+  assetWithDecimal: { asset, decimal: assetDecimal },
+  network
+}: GetLiquidityProvidersParams): LiquidityProvidersLD => {
+  const poolString = assetToString(asset)
+  return FP.pipe(
+    reloadLiquidityProviders$,
+    RxOp.switchMap(() => thorNodeApiAddress$(network)),
+    liveData.chain((api) =>
+      FP.pipe(
+        api,
+        (thorApi) => RxAjax.ajax.getJSON(`${thorApi}/pool/${poolString}/liquidity_providers`),
+        // We can not use something like t.array(LiquidityProviderIO) as in case if one of elements
+        // fails validation the WHOLE array will be processed as failed-validation by io-ts.
+        // So we need to validate each elemnt "by-hands" to avoid losing all data 'cuz of 1 element's fail
+
+        // First check - check if we have an array itself at the response
+        RxOp.map(t.array(t.unknown).decode),
+        RxOp.map(E.fold(() => [], FP.identity)),
+        // Secondly - check every response's element with LiquidityProviderIO codec and filter out every failed-validation-element
+        RxOp.map(A.filterMap(FP.flow(LiquidityProviderIO.decode, O.fromEither))),
+        RxOp.map(RD.success),
+        liveData.map(
+          A.map((provider) => ({
+            asset: provider.asset,
+            address: provider.rune_address,
+            assetAddress: provider.asset_address,
+            pendingRune: baseAmount(provider.pending_rune, THORCHAIN_DECIMAL),
+            pendingAsset: baseAmount(provider.pending_asset, assetDecimal)
+          }))
+        ),
+        RxOp.catchError(
+          (): LiquidityProvidersLD =>
+            Rx.of(
+              RD.failure({
+                errorId: ErrorId.GET_LIQUIDITY_PROVIDERS,
+                msg: `Failed to load info for ${poolString} pool`
+              })
+            )
+        )
+      )
+    ),
+
+    RxOp.startWith(RD.pending)
+  )
+}
+
+const getLiquidityProvider = ({
+  assetWithDecimal,
+  network,
+  runeAddress,
+  assetAddress
+}: GetLiquidityProviderParams): LiquidityProviderLD =>
+  FP.pipe(
+    getLiquidityProviders({
+      assetWithDecimal,
+      network
+    }),
+    liveData.map(
+      A.findFirst(
+        (provider) =>
+          eqOString.equals(provider.address, runeAddress) && eqOString.equals(provider.assetAddress, assetAddress)
+      )
+    )
+  )
+
 const { stream$: reloadMimir$, trigger: reloadMimir } = triggerStream()
 
 const mimir$: MimirLD = FP.pipe(
@@ -116,7 +199,7 @@ const mimir$: MimirLD = FP.pipe(
       RxOp.map((response) => MimirIO.decode(response)),
       RxOp.map((result) =>
         // Errors -> Error
-        E.mapLeft((_: Errors) => Error(`Failed loading mimir ${PathReporter.report(result)}`))(result)
+        E.mapLeft((_: t.Errors) => Error(`Failed loading mimir ${PathReporter.report(result)}`))(result)
       ),
       RxOp.map(RD.fromEither),
       RxOp.startWith(RD.pending)
@@ -124,4 +207,4 @@ const mimir$: MimirLD = FP.pipe(
   )
 )
 
-export { getNodeInfo$, reloadNodesInfo, mimir$, reloadMimir }
+export { getNodeInfo$, reloadNodesInfo, mimir$, reloadMimir, getLiquidityProvider, reloadLiquidityProviders }
--- src/renderer/services/thorchain/types.ts
+++ src/renderer/services/thorchain/types.ts
@@ -1,11 +1,18 @@
 import * as RD from '@devexperts/remote-data-ts'
-import { TxHash } from '@xchainjs/xchain-client'
+import { Address, TxHash } from '@xchainjs/xchain-client'
 import { Client, DepositParam } from '@xchainjs/xchain-thorchain'
-import { Asset, BaseAmount } from '@xchainjs/xchain-util'
+import { Asset, assetFromString, BaseAmount, isValidAsset } from '@xchainjs/xchain-util'
+import * as E from 'fp-ts/Either'
+import * as FP from 'fp-ts/function'
+import * as O from 'fp-ts/Option'
 import * as t from 'io-ts'
+import { optionFromNullable } from 'io-ts-types/lib/optionFromNullable'
+import * as D from 'io-ts/Decoder'
 import * as Rx from 'rxjs'
 
+import { Network } from '../../../shared/api/types'
 import { LiveData } from '../../helpers/rx/liveData'
+import { AssetWithDecimal } from '../../types/asgardex'
 import * as C from '../clients'
 import { ApiError, TxHashLD } from '../wallet/types'
 
@@ -70,3 +77,79 @@ export type Mimir = t.TypeOf<typeof MimirIO>
 
 export type MimirLD = LiveData<Error, Mimir>
 export type MimirRD = RD.RemoteData<Error, Mimir>
+
+export type GetLiquidityProvidersParams = {
+  assetWithDecimal: AssetWithDecimal
+  network: Network
+}
+
+export type GetLiquidityProviderParams = GetLiquidityProvidersParams & {
+  runeAddress: O.Option<Address>
+  assetAddress: O.Option<Address>
+}
+
+export type LiquidityProvider = {
+  asset: Asset
+  address: O.Option<Address>
+  assetAddress: O.Option<Address>
+  pendingRune: BaseAmount
+  pendingAsset: BaseAmount
+}
+
+export type LiquidityProvidersLD = LiveData<ApiError, LiquidityProvider[]>
+export type LiquidityProviderLD = LiveData<ApiError, O.Option<LiquidityProvider>>
+export type LiquidityProviderRD = RD.RemoteData<ApiError, O.Option<LiquidityProvider>>
+
+const assetDecoder: D.Decoder<unknown, Asset> = FP.pipe(
+  D.string,
+  D.parse((stringAsset) =>
+    FP.pipe(
+      stringAsset,
+      assetFromString,
+      E.fromNullable(new Error('Invalid asset')),
+      E.fold((e) => D.failure(stringAsset, e.message), D.success)
+    )
+  )
+)
+
+/**
+ * Custom type validator to check if string is Asset instance
+ * After successful validation transforms string to Asset
+ */
+const assetType = new t.Type(
+  'asset type',
+  (input): input is Asset => {
+    if (typeof input === 'string') {
+      const asset = assetFromString(input)
+
+      if (asset) {
+        return isValidAsset(asset)
+      }
+    }
+    return false
+  },
+  (input, context) => {
+    if (typeof input === 'string') {
+      return FP.pipe(
+        assetFromString(input),
+        E.fromNullable(Error('Invalid asset')),
+        E.fold(
+          () => t.failure(input, context),
+          (asset) => t.success(asset)
+        )
+      )
+    }
+    return t.failure(input, context)
+  },
+  assetDecoder.decode
+)
+
+export const LiquidityProviderIO = t.type({
+  asset: assetType,
+  rune_address: optionFromNullable(t.string),
+  asset_address: optionFromNullable(t.string),
+  pending_rune: t.string,
+  pending_asset: t.string,
+  rune_deposit_value: t.string,
+  asset_deposit_value: t.string
+})
