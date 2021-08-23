e411f76565b31334615365e3f82d87ddee4abe2e
Veado <veado@protonmail.com>
2021-08-23 15:15:10 +0200

    Update story

--- src/renderer/components/swap/CustomAddressInput.stories.tsx
+++ src/renderer/components/swap/CustomAddressInput.stories.tsx
@@ -1,14 +1,19 @@
 import { Story, Meta } from '@storybook/react'
+import { Address } from '@xchainjs/xchain-client'
 import { AssetBNB } from '@xchainjs/xchain-util'
 
+import { BNB_ADDRESS_TESTNET } from '../../../shared/mock/address'
+import { eqString } from '../../helpers/fp/eq'
 import { CustomAddressInput, CustomAddressInputProps } from './CustomAddressInput'
+
+const bnbAddress = BNB_ADDRESS_TESTNET
 
 const defaultProps: CustomAddressInputProps = {
   asset: AssetBNB,
-  address: 'tbnb123123123123',
+  address: bnbAddress,
   onClickOpenAddress: () => console.log('open address in explorer'),
   onChangeAddress: () => console.log('address changed'),
-  addressValidator: () => true
+  addressValidator: (address: Address) => eqString.equals(address, bnbAddress)
 }
 export const StoryDefault: Story = () => <CustomAddressInput {...defaultProps} />
 StoryDefault.storyName = 'default'
