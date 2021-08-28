a0e67c3c08b37b1a83a92e1bfa31c81f47922705
Veado <veado@protonmail.com>
2021-08-28 15:21:21 +0200

    Fix `useOpenExplorerTxUrl` to observe chain
    changes more explicit

--- src/renderer/hooks/useOpenExplorerTxUrl.ts
+++ src/renderer/hooks/useOpenExplorerTxUrl.ts
@@ -1,4 +1,4 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 import { TxHash, XChainClient } from '@xchainjs/xchain-client'
 import { Chain } from '@xchainjs/xchain-util'
@@ -6,21 +6,28 @@ import * as FP from 'fp-ts/function'
 import * as O from 'fp-ts/Option'
 import { useObservableState } from 'observable-hooks'
 import * as Rx from 'rxjs'
+import * as RxOp from 'rxjs/operators'
 
 import { useChainContext } from '../contexts/ChainContext'
+import { eqOChain } from '../helpers/fp/eq'
 import { OpenExplorerTxUrl } from '../services/clients'
 
 export const useOpenExplorerTxUrl = (oChain: O.Option<Chain>): OpenExplorerTxUrl => {
   const { clientByChain$ } = useChainContext()
 
-  const [oClient] = useObservableState<O.Option<XChainClient>>(
-    () =>
+  const [oClient, chainUpdated] = useObservableState<O.Option<XChainClient>, O.Option<Chain>>(
+    (oChain$) =>
       FP.pipe(
-        oChain,
-        O.fold(() => Rx.EMPTY, clientByChain$)
+        oChain$,
+        RxOp.distinctUntilChanged(eqOChain.equals) /* compare prev./current value - just for performance reason */,
+        RxOp.switchMap(FP.flow(O.fold(() => Rx.EMPTY, clientByChain$)))
       ),
     O.none
   )
+
+  // `chainUpdated` needs to be called whenever chain has been updated
+  // to trigger `useObservableState` properly to get a client depending on chain
+  useEffect(() => chainUpdated(oChain), [oChain, chainUpdated])
 
   const openExplorerTxUrl: OpenExplorerTxUrl = useCallback(
     (txHash: TxHash) =>
