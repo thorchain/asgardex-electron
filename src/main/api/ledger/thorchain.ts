97da44c46d53dae32a6b36006ab671b75d0ee588
grōm <87367763+gromxyz@users.noreply.github.com>
2021-09-01 20:17:03 +0200

    Resolve: Extract `toClientNetwork` (#1719)
    * Extract toClientNetwork to shared
    
    * Remove old toClientNetwork
    
    * Move tests

--- src/main/api/ledger/thorchain.ts
+++ src/main/api/ledger/thorchain.ts
@@ -1,15 +1,12 @@
 import type Transport from '@ledgerhq/hw-transport'
 import THORChainApp, { LedgerErrorType } from '@thorchain/ledger-thorchain'
-import * as Client from '@xchainjs/xchain-client'
 import { getPrefix } from '@xchainjs/xchain-thorchain'
 import * as E from 'fp-ts/Either'
 
 import { LedgerErrorId, Network } from '../../../shared/api/types'
+import { toClientNetwork } from '../../../shared/utils/client'
 import { getErrorId } from './utils'
 
-// TODO(@Veado) Move `toClientNetwork` from `renderer/services/clients` to `main/util` or so
-const toClientNetwork = (network: Network): Client.Network =>
-  network === 'mainnet' ? Client.Network.Mainnet : Client.Network.Testnet
 // TODO(@veado) Get path by using `xchain-thorchain`
 const PATH = [44, 931, 0, 0, 0]
 
