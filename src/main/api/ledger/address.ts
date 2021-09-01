9db3a5944bd4b7f8581751bd4ff25e13a29069e8
gromxyz <grom0x183@gmail.com>
2021-09-01 16:09:03 +0200

    Fix network type dependencies

--- src/main/api/ledger/address.ts
+++ src/main/api/ledger/address.ts
@@ -1,8 +1,9 @@
 import TransportNodeHidSingleton from '@ledgerhq/hw-transport-node-hid-singleton'
+import { Network } from '@xchainjs/xchain-client/lib/types'
 import { BNBChain, Chain, THORChain } from '@xchainjs/xchain-util'
 import * as E from 'fp-ts/Either'
 
-import { LedgerErrorId, Network } from '../../../shared/api/types'
+import { LedgerErrorId } from '../../../shared/api/types'
 import { getAddress as getBNBAddress } from './binance'
 import { getAddress as getTHORAddress } from './thorchain'
 import { getErrorId } from './utils'
