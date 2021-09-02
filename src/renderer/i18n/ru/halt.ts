123873ecbcb31fd3364fbab48cbed7a91b93e752
gromxyz <grom0x183@gmail.com>
2021-09-02 12:32:02 +0200

    Update RU translation

--- src/renderer/i18n/ru/halt.ts
+++ src/renderer/i18n/ru/halt.ts
@@ -5,7 +5,7 @@ const halt: HaltMessages = {
   'halt.trading': 'Торговля по всем пулам временно приостановлена.',
   'halt.trading.eth': 'Торговля ETH временно приостановлена.',
   'halt.chain': 'Торговля для цепи(ей) {chain} приостановлена.',
-  'halt.chain.upgrade': 'Upgrade for {chain} is disabled for maintenance temporarily. - RU'
+  'halt.chain.upgrade': 'Обновление для {chain} временно приостановлено для поддержки.'
 }
 
 export default halt
