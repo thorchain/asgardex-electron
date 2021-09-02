3615fd7ab8e0e883e8207271ebf1bdfe1962c203
gromxyz <grom0x183@gmail.com>
2021-09-02 14:17:28 +0200

    Update halt upgrade DE translation

--- src/renderer/i18n/de/halt.ts
+++ src/renderer/i18n/de/halt.ts
@@ -5,7 +5,7 @@ const halt: HaltMessages = {
   'halt.trading': 'Der Handel ist vorübergehend für alle Pools gestoppt.',
   'halt.trading.eth': 'Der Handel für ETH ist vorübergehend gestoppt.',
   'halt.chain': 'Der Handel in Pools von {chain} Chain(s) ist gestoppt aufgrund von Wartungsarbeiten.',
-  'halt.chain.upgrade': 'Upgrade for {chainTx} is disabled for maintenance temporarily. - DE'
+  'halt.chain.upgrade': 'Upgrade für {chainTx} ist vorübergehend deaktiviert.'
 }
 
 export default halt
