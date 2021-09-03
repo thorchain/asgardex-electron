2ca8b2dd9528170ecb41373c38bffb179ce2ed48
Veado <veado@protonmail.com>
2021-09-03 15:29:47 +0200

    Update translation

--- src/renderer/i18n/de/halt.ts
+++ src/renderer/i18n/de/halt.ts
@@ -3,8 +3,8 @@ import { HaltMessages } from '../types'
 const halt: HaltMessages = {
   'halt.thorchain': 'THORChain wurde vorübergehend angehalten.',
   'halt.trading': 'Der Handel ist vorübergehend für alle Pools gestoppt.',
-  'halt.chain': 'Der Handel in Pools von {chains} Chain(s) ist gestoppt aufgrund von Wartungsarbeiten.',
-  'halt.chain.trading': 'Der Handel für {chains} ist vorübergehend gestoppt.',
+  'halt.chain': 'Der Handel in Pools von {chains} Chain(s) wurde aufgrund von Wartungsarbeiten vorübergehend gestoppt.',
+  'halt.chain.trading': 'Der Handel für {chains} Chain(s) wurde vorübergehend gestoppt.',
   'halt.chain.upgrade': 'Upgrade für {chains} ist vorübergehend deaktiviert.'
 }
 
