6229a2e26eb4e42a44f5b03b1015ece5be441bed
Veado <veado@protonmail.com>
2021-09-07 20:05:53 +0200

    More tests for guards

--- src/shared/api/guard.test.ts
+++ src/shared/api/guard.test.ts
@@ -1,6 +1,6 @@
-import { AssetRuneNative } from '@xchainjs/xchain-util'
+import { AssetRuneNative, baseAmount, bn } from '@xchainjs/xchain-util'
 
-import { isAsset } from './guard'
+import { isAsset, isBaseAmount, isChain, isNetwork } from './guard'
 
 describe('shared/guard', () => {
   describe('isAsset', () => {
@@ -15,6 +15,45 @@ describe('shared/guard', () => {
     })
     it('false for invalid string ', () => {
       expect(isAsset('invalid')).toBeFalsy()
+    })
+  })
+
+  describe('isChain', () => {
+    it('true for "THOR"', () => {
+      expect(isChain('THOR')).toBeTruthy()
+    })
+    it('true for ETH', () => {
+      expect(isChain('ETH')).toBeTruthy()
+    })
+    it('false for invalid chain ', () => {
+      expect(isChain('INVALID')).toBeFalsy()
+    })
+  })
+
+  describe('isNetwork', () => {
+    it('true for "mainnet"', () => {
+      expect(isNetwork('mainnet')).toBeTruthy()
+    })
+    it('true for testnet', () => {
+      expect(isNetwork('testnet')).toBeTruthy()
+    })
+    it('false for invalid network ', () => {
+      expect(isNetwork('network')).toBeFalsy()
+    })
+  })
+
+  describe('isBaseAmount', () => {
+    it('true for "THOR"', () => {
+      expect(isBaseAmount(baseAmount(1))).toBeTruthy()
+    })
+    it('false for BigNumber', () => {
+      expect(isBaseAmount(bn('123'))).toBeFalsy()
+    })
+    it('false for string', () => {
+      expect(isBaseAmount('123')).toBeFalsy()
+    })
+    it('false for number ', () => {
+      expect(isBaseAmount(2)).toBeFalsy()
     })
   })
 })
