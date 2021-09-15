98a63c989ebcdc49807d3c652a76c31cc61cfeb7
gromxyz <grom0x183@gmail.com>
2021-09-15 13:24:56 +0200

    Shorten expression

--- src/renderer/components/uielements/info/InfoIcon.stories.tsx
+++ src/renderer/components/uielements/info/InfoIcon.stories.tsx
@@ -4,6 +4,4 @@ import { storiesOf } from '@storybook/react'
 
 import { InfoIcon } from './InfoIcon'
 
-storiesOf('Components/InfoIcon', module).add('default', () => {
-  return <InfoIcon tooltip="testing info icon" />
-})
+storiesOf('Components/InfoIcon', module).add('default', () => <InfoIcon tooltip="testing info icon" />)
