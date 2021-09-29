6b471eda1db109c0272ae13cd63067209fa53832
Weyland2093 <79262935+Weyland2093@users.noreply.github.com>
2021-09-29 14:14:58 +0200

    Update halt.ts

--- src/renderer/i18n/fr/halt.ts
+++ src/renderer/i18n/fr/halt.ts
@@ -3,10 +3,10 @@ import { HaltMessages } from '../types'
 const halt: HaltMessages = {
   'halt.thorchain': 'THORChain est temporairement interrompu.',
   'halt.trading': 'L’échange sur toutes les pools de liquidité est temporairement interrompu.',
-  'halt.chain': '{chain} chain has been halted temporarily - FR',
-  'halt.chains': "L'échange pour les actifs des chaînes {chains} est interrompu pour maintenance",
-  'halt.chain.pause': 'Liquidity activities (add/remove) for {chains} chain(s) have been disabled temporarily - FR.',
+  'halt.chain': 'La chaîne {chain} est temporairement interrompue.',
+  'halt.chains': "Les chaînes {chains} sont temporairement interrompues.",
   'halt.chain.trading': 'L’échange pour {chains} est temporairement interrompu.',
+  'halt.chain.pause': 'Les activités de liquidité (ajouter/retirer) pour la(es) chaîne(s) {chains} sont temporairement désactivées.',
   'halt.chain.upgrade': 'La mise à jour pour {chains} est temporairement désactivée pour maintenance.'
 }
 
