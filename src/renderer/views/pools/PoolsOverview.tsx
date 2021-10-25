727484cb1465dbfd815ae9c1d43c1ec373c6353c
grōm <87367763+gromxyz@users.noreply.github.com>
2021-10-25 15:29:50 +0200

    [Pools] Disable manage button while wallet locked (#1877)

--- src/renderer/views/pools/PoolsOverview.tsx
+++ src/renderer/views/pools/PoolsOverview.tsx
@@ -3,12 +3,15 @@ import React, { useCallback, useMemo, useState } from 'react'
 import * as RD from '@devexperts/remote-data-ts'
 import { Chain } from '@xchainjs/xchain-util'
 import * as FP from 'fp-ts/function'
+import * as O from 'fp-ts/lib/Option'
 import { useObservableState } from 'observable-hooks'
 import { useIntl } from 'react-intl'
 import * as RxOp from 'rxjs/operators'
 
 import { useMidgardContext } from '../../contexts/MidgardContext'
+import { useWalletContext } from '../../contexts/WalletContext'
 import { useMimirHalt } from '../../hooks/useMimirHalt'
+import { isLocked } from '../../services/wallet/util'
 import { ActivePools } from './ActivePools'
 import { PendingPools } from './PendingPools'
 import * as Styled from './PoolsOverview.styles'
@@ -23,6 +26,9 @@ type Tab = {
 
 export const PoolsOverview: React.FC = (): JSX.Element => {
   const intl = useIntl()
+
+  const { keystoreService } = useWalletContext()
+  const keystore = useObservableState(keystoreService.keystore$, O.none)
 
   const [activeTabKey, setActiveTabKey] = useState('active')
 
@@ -41,15 +47,15 @@ export const PoolsOverview: React.FC = (): JSX.Element => {
       {
         key: 'active',
         label: intl.formatMessage({ id: 'pools.available' }),
-        content: <ActivePools haltedChains={haltedChains} mimirHalt={mimirHalt} />
+        content: <ActivePools haltedChains={haltedChains} mimirHalt={mimirHalt} walletLocked={isLocked(keystore)} />
       },
       {
         key: 'pending',
         label: intl.formatMessage({ id: 'pools.pending' }),
-        content: <PendingPools haltedChains={haltedChains} mimirHalt={mimirHalt} />
+        content: <PendingPools haltedChains={haltedChains} mimirHalt={mimirHalt} walletLocked={isLocked(keystore)} />
       }
     ],
-    [intl, haltedChains, mimirHalt]
+    [intl, haltedChains, mimirHalt, keystore]
   )
 
   const renderTabBar = useCallback(
