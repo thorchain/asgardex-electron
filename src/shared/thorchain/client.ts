dc192c6cecb66c53d2b6f10a3712a89935ff992b
Polaris <polaris@ninerealms.co>
2021-12-30 11:22:53 -0800

    Uncomment stagenet changes, update package libraries for xchainjs

--- src/shared/thorchain/client.ts
+++ src/shared/thorchain/client.ts
@@ -5,19 +5,17 @@ import { envOrDefault } from '../utils/env'
 
 export const getClientUrl = (): ClientUrl => {
   const { node: nodeTestnet, rpc: rpcTestnet } = getDefaultClientUrl()[Network.Testnet]
-  // TODO: uncomment this when we can update xchainjs
-  // const { node: nodeStagenet, rpc: rpcStagenet } = getDefaultClientUrl()[Network.Stagenet]
+  const { node: nodeStagenet, rpc: rpcStagenet } = getDefaultClientUrl()[Network.Stagenet]
   const { node: nodeMainnet, rpc: rpcMainnet } = getDefaultClientUrl()[Network.Mainnet]
   return {
     [Network.Testnet]: {
       node: envOrDefault(process.env.REACT_APP_TESTNET_THORNODE_API, nodeTestnet),
       rpc: envOrDefault(process.env.REACT_APP_TESTNET_THORNODE_RPC, rpcTestnet)
     },
-    // TODO: uncomment this when we can update xchainjs
-    // [Network.Stagenet]: {
-    //   node: envOrDefault(process.env.REACT_APP_STAGENET_THORNODE_API, nodeStagenet),
-    //   rpc: envOrDefault(process.env.REACT_APP_STAGENET_THORNODE_RPC, rpcStagenet)
-    // },
+    [Network.Stagenet]: {
+      node: envOrDefault(process.env.REACT_APP_STAGENET_THORNODE_API, nodeStagenet),
+      rpc: envOrDefault(process.env.REACT_APP_STAGENET_THORNODE_RPC, rpcStagenet)
+    },
     [Network.Mainnet]: {
       node: envOrDefault(process.env.REACT_APP_MAINNET_THORNODE_API, nodeMainnet),
       rpc: envOrDefault(process.env.REACT_APP_MAINNET_THORNODE_RPC, rpcMainnet)
--- src/shared/utils/client.ts
+++ src/shared/utils/client.ts
@@ -2,5 +2,13 @@ import * as Client from '@xchainjs/xchain-client'
 
 import { Network } from '../api/types'
 
-export const toClientNetwork = (network: Network): Client.Network =>
-  network === 'mainnet' ? Client.Network.Mainnet : Client.Network.Testnet
+export const toClientNetwork = (network: Network): Client.Network => {
+  switch (network) {
+    case 'mainnet':
+      return Client.Network.Mainnet
+    case 'stagenet':
+      return Client.Network.Stagenet
+    case 'testnet':
+      return Client.Network.Testnet
+  }
+}
