ac7e008e64c2104d78a54385375dd53cbfd44f76
TreefeedXavier <TreefeedXavier@users.noreply.github.com>
2022-01-19 16:31:17 +0200

    updating ru translation (#2032)

--- src/renderer/i18n/ru/swap.ts
+++ src/renderer/i18n/ru/swap.ts
@@ -1,26 +1,26 @@
 import { SwapMessages } from '../types'
 
 const swap: SwapMessages = {
-  'swap.state.pending': 'Обменять',
+  'swap.state.pending': 'Обмениваю',
   'swap.state.success': 'Обмен совершён',
   'swap.state.error': 'Ошибка при обмене',
   'swap.input': 'Отдаете',
   'swap.output': 'Получаете',
-  'swap.recipient': 'Recipient - RU',
-  'swap.info.max.fee': 'Total asset balance substracted by estimated swap fees - RU',
+  'swap.recipient': 'Получатель',
+  'swap.info.max.fee': 'Баланс актива за вычетом комиссии обмена',
   'swap.slip.title': 'Проскальзывание',
   'swap.slip.tolerance': 'Допуск по проскальзыванию',
   'swap.slip.tolerance.info':
-    'The higher the percentage, the more slippage you will accept. More slippage includes also a wider range for covering estimated fees to avoid aborted swaps. - RU',
+    'Чем выше процент, тем большее проскальзывание вы допускаете. Большее проскальзывание включает также более широкий диапазон расчёта комиссий во избежание прерывания обмена.',
   'swap.errors.amount.balanceShouldCoverChainFee':
     'Комиссия транзакции {fee} дожна покрываться вашим балансом (сейчас {balance}).',
   'swap.errors.amount.outputShouldCoverChainFee':
     'Исходящая комиссия {fee} должна покрываться получаемым количеством (сейчас {amount}).',
   'swap.note.lockedWallet': 'Для обмена необходимо разблокировать кошелек',
   'swap.note.nowallet': 'Для обмена создайте или импортируйте кошелек',
-  'swap.errors.asset.missingSourceAsset': 'Исходный ассет не поддерживается',
-  'swap.errors.asset.missingTargetAsset': 'Конечный ассет не поддерживается',
-  'swap.ledger.sign': 'Click next to sign the swap transaction on your device. - RU'
+  'swap.errors.asset.missingSourceAsset': 'Исходный актив не поддерживается',
+  'swap.errors.asset.missingTargetAsset': 'Целевой актив не поддерживается',
+  'swap.ledger.sign': 'Нажмите далее что бы подписать транзакцию на вашем устройстве.'
 }
 
 export default swap
