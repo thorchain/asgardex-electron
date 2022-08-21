0601652704051f39e91f026766012ee50d5f74e7
TreefeedXavier <TreefeedXavier@users.noreply.github.com>
2022-08-21 17:53:46 +0300

    Update midgard.ts

--- src/renderer/i18n/ru/midgard.ts
+++ src/renderer/i18n/ru/midgard.ts
@@ -3,9 +3,9 @@ import { MidgardMessages } from '../types'
 const midgard: MidgardMessages = {
   'midgard.error.byzantine.title': 'Ошибка модуля Midgard Byzantine',
   'midgard.error.byzantine.description': 'Не удалось достичь консенсуса во время поиска эндпоинта для Midgard АПИ',
-  'midgard.url.error.invalid': 'Invalid Midgard URL. Please double check it and try it again - RU',
-  'midgard.url.error.unhealthy': 'Midgard seems to be unhealthy by checking "{endpoint}" - RU',
-  'midgard.url.valid': 'Valid Midgard URL - RU'
+  'midgard.url.error.invalid': 'Неверный URL Midgard. Проверьте ещё раз и возвращайтесь',
+  'midgard.url.error.unhealthy': 'Midgard выдаёт дичь при проверке "{endpoint}"',
+  'midgard.url.valid': 'Действительный URl Midgard'
 }
 
 export default midgard
