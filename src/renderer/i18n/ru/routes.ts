6d2993b7959220c72bb0c062b37a22ad3ba4768f
TreefeedXavier <TreefeedXavier@users.noreply.github.com>
2022-08-21 17:50:56 +0300

    Update routes.ts

--- src/renderer/i18n/ru/routes.ts
+++ src/renderer/i18n/ru/routes.ts
@@ -1,7 +1,7 @@
 import { RoutesMessages } from '../types'
 
 const messages: RoutesMessages = {
-  'routes.invalid.asset': 'Недопустимый актив в пути: {asset}',
+  'routes.invalid.asset': 'Недопустимый актив в маршруте: {asset}',
   'routes.invalid.params': 'Недопустимый параметр(ы) маршрута {params}'
 }
 
