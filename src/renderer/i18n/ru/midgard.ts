f1d905a5d55b41508ad1368c4cd2325126f37651
TreefeedXavier <TreefeedXavier@users.noreply.github.com>
2022-09-28 10:38:21 +0300

    Update midgard.ts

--- src/renderer/i18n/ru/midgard.ts
+++ src/renderer/i18n/ru/midgard.ts
@@ -4,7 +4,7 @@ const midgard: MidgardMessages = {
   'midgard.error.endpoint.title': 'Ошибка конечного узла Midgard',
   'midgard.url.error.invalid': 'Неверный URL Midgard. Проверьте ещё раз и возвращайтесь',
   'midgard.url.error.unhealthy': 'Midgard выдаёт дичь при проверке "{endpoint}"',
-  'midgard.url.valid': 'Действительный URl Midgard'
+  'midgard.url.valid': 'Действительный URL Midgard'
 }
 
 export default midgard
