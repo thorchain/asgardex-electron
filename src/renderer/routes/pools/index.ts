d450f737c72a9850196b499f8ddcfec2ad181b16
Veado <veado@protonmail.com>
2022-11-09 16:21:44 +0100

    [Savers] Routes

--- src/renderer/routes/pools/index.ts
+++ src/renderer/routes/pools/index.ts
@@ -1,1 +1,11 @@
-export * from './pools'
+import * as baseRoutes from './base'
+import * as depositRoutes from './deposit'
+import * as poolDetailRoutes from './detail'
+import * as saversRoutes from './savers'
+import * as swapRoutes from './swap'
+
+export const base = baseRoutes.base
+export const deposit = depositRoutes.deposit
+export const savers = saversRoutes.savers
+export const swap = swapRoutes.swap
+export const detail = poolDetailRoutes.poolDetail
