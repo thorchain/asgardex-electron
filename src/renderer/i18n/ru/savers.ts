6b4e50bd376aa2dc139f74d832f78c28785a0ce9
sergey.soltys <sergey.soltys@eleks.com>
2022-12-20 22:55:15 +0200

    - RU translation for savers.ts - minor fixes

--- src/renderer/i18n/ru/savers.ts
+++ src/renderer/i18n/ru/savers.ts
@@ -1,7 +1,7 @@
 import { SaversMessages } from '../types'
 
 const savers: SaversMessages = {
-  'savers.noSavings': "У тебя нет сбережений",
+  'savers.noSavings': 'У тебя нет сбережений',
   'savers.detail.title': 'Твои сбережения',
   'savers.detail.current.title': 'Сколько внесено',
   'savers.detail.redeem.title': 'Сколько заберёшь',
